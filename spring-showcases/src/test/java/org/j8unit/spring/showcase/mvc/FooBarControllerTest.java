9e9c1f069c982a79638a6fd04404b686fba4febd
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-22 13:53:56 +0200

    J8Unit als Core-Bestandteil verschoben ins Core-Projekt

--- spring-showcases/src/test/java/org/j8unit/spring/showcase/mvc/FooBarControllerTest.java
+++ spring-showcases/src/test/java/org/j8unit/spring/showcase/mvc/FooBarControllerTest.java
@@ -1,10 +1,11 @@
 package org.j8unit.spring.showcase.mvc;
 
-import org.j8unit.repository.J8UnitTest;
+import org.j8unit.J8UnitTest;
 import org.junit.Assert;
 import org.junit.Test;
 
-public interface FooBarControllerTest extends J8UnitTest<FooBarController> {
+public interface FooBarControllerTest
+extends J8UnitTest<FooBarController> {
 
     @Test
     public default void testFoo() {
