831aa49440e99996ed4cca5e7e19cc0fc34b6add
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-22 21:43:20 +0200

    Formatter

--- showcases/src/test/java/org/j8unit/showcase4/AllFoobarTests.java
+++ showcases/src/test/java/org/j8unit/showcase4/AllFoobarTests.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ org.j8unit.showcase1a.FoobarTest.class, org.j8unit.showcase1b.FoobarTest.class, org.j8unit.showcase1c.FoobarTest.class,
-               org.j8unit.showcase2a.FoobarTest.class, org.j8unit.showcase2b.FoobarTest.class, org.j8unit.showcase2c.FoobarTest.class,
-               org.j8unit.showcase3a.FoobarTest.class, org.j8unit.showcase3b.FoobarTest.class, org.j8unit.showcase3c.FoobarTest.class })
+                org.j8unit.showcase2a.FoobarTest.class, org.j8unit.showcase2b.FoobarTest.class, org.j8unit.showcase2c.FoobarTest.class,
+                org.j8unit.showcase3a.FoobarTest.class, org.j8unit.showcase3b.FoobarTest.class, org.j8unit.showcase3c.FoobarTest.class })
 public class AllFoobarTests {
 
 }
