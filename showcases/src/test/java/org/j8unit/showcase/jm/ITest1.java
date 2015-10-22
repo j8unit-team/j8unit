d93ecb979ccd846ce444f741c46478fc202631d2
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-22 21:42:08 +0200

    Simplifizierung Showcase-Test1

--- showcases/src/test/java/org/j8unit/showcase/jm/ITest1.java
+++ showcases/src/test/java/org/j8unit/showcase/jm/ITest1.java
@@ -1,28 +1,19 @@
 package org.j8unit.showcase.jm;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
 public interface ITest1 {
 
     @Test
-    public default void testConcat() {
-        final List<String> sut = new ArrayList<>();
-        final int before = sut.size();
-        sut.add("foobar");
-        final int after = sut.size();
-        Assert.assertEquals(before + 1, after);
-        Assert.assertTrue(sut.contains("foobar"));
+    public default void testFoo() {
+        // ...
+        Assert.assertTrue(true);
     }
 
     @Test
-    public default void testClear() {
-        final List<String> sut = new ArrayList<>();
-        sut.clear();
-        Assert.assertEquals(0, sut.size());
-        Assert.assertTrue(sut.isEmpty());
+    public default void testBar() {
+        // ...
+        Assert.assertTrue(true);
     }
-
 }
--- showcases/src/test/java/org/j8unit/showcase/jm/Test1.java
+++ showcases/src/test/java/org/j8unit/showcase/jm/Test1.java
@@ -1,11 +1,17 @@
 package org.j8unit.showcase.jm;
 
 import org.j8unit.runners.J8Unit4;
-import org.j8unit.showcase.jm.ITest1;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(J8Unit4.class)
 public class Test1
 implements ITest1 {
 
+    @Test
+    public void testFoobar() {
+        // ...
+        Assert.assertTrue(true);
+    }
 }
