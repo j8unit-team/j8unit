29fee977f0dafc4146c390802337e1d123d39ab3
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-22 18:55:09 +0200

    Zusatz-Query-Test für unmodifiable List

--- repository/src/main/java/org/j8unit/repository/java/util/UnmodifiableListTests.java
+++ repository/src/main/java/org/j8unit/repository/java/util/UnmodifiableListTests.java
@@ -1,8 +1,24 @@
 package org.j8unit.repository.java.util;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.UnaryOperator;
+import org.junit.Test;
 
 @FunctionalInterface
 public abstract interface UnmodifiableListTests<L extends List<E>, E>
 extends UnmodifiableCollectionTests<L, E>, ListTests<L, E> {
+
+    @Test(expected = UnsupportedOperationException.class)
+    public default void testForbiddenReplaceAll() {
+        final L sut = this.createNewSUT();
+        sut.replaceAll(UnaryOperator.identity());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public default void testForbiddenSort() {
+        final L sut = this.createNewSUT();
+        sut.sort(Comparator.comparing(Object::toString));
+    }
+
 }
