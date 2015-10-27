aba7cd6afb218970923377c0b2f05904645f9d05
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-27 09:53:14 +0100

    Fix: Showcase-OldStyleTest1 is a class

--- showcases-jm/src/test/java/org/j8unit/showcase/jm/OldStyleTest1.java
+++ showcases-jm/src/test/java/org/j8unit/showcase/jm/OldStyleTest1.java
@@ -2,16 +2,16 @@ package org.j8unit.showcase.jm;
 
 import org.junit.Test;
 
-public interface OldStyleTest1 {
+public class OldStyleTest1 {
 
     @Test
-    public default void testFoobar1() {
+    public void testFoobar1() {
         final Foobar sut = new Foobar();
         ReusableTests.testFoobar1(sut);
     }
 
     @Test
-    public default void testFoobar2() {
+    public void testFoobar2() {
         final Foobar sut = new Foobar();
         ReusableTests.testFoobar2(sut);
     }
