66feab20621c353de46d946f4b103f752c7a2d9c
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-27 12:09:19 +0100

    Javadoc++ (J8UnitTest)

--- core/src/main/java/org/j8unit/J8UnitTest.java
+++ core/src/main/java/org/j8unit/J8UnitTest.java
@@ -1,11 +1,23 @@
 package org.j8unit;
 
 /**
- * Base interface of any subject-under-test based J8Unit test.
+ * <p>
+ * Base interface of any subject-under-test based J8Unit test. Extending this {@code interface} enables specific test
+ * definitions to {@linkplain #createNewSUT() query a fresh subject-under-test instance}.
+ * </p>
+ *
+ * <p>
+ * This behaviour is somehow similar to JUnit's {@link org.junit.Before} used to re-initialise {@code class}' members.
+ * However, interfaces can specify methods but cannot specify members. Thus, specifying a subject-under-test factory
+ * method is the way it works.
+ * </p>
+ *
+ * @see FactoryBasedJ8UnitTest
  *
  * @param <SUT>
  *            the type of the subject-under-test
  */
+@FunctionalInterface
 public abstract interface J8UnitTest<SUT> {
 
     /**
