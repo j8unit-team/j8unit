724cd3aeb43db218e0b0708fcf460910a8512d58
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-31 00:20:55 +0100

    Disable ListWrapperTestCase because of Oracle/Java's bugs

--- repository/src/test/java/org/j8unit/repository/java/util/ListWrapperTestCase.java
+++ repository/src/test/java/org/j8unit/repository/java/util/ListWrapperTestCase.java
@@ -10,11 +10,13 @@ import org.j8unit.FactoryBasedJ8UnitTest;
 import org.j8unit.repository.java.lang.ObjectTests;
 import org.j8unit.runners.J8Parameterized;
 import org.j8unit.runners.parameterized.J8BlockJUnit4ClassRunnerWithParametersFactory;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
+@Ignore("Java has some bugs ;-(")
 @RunWith(J8Parameterized.class)
 @UseParametersRunnerFactory(J8BlockJUnit4ClassRunnerWithParametersFactory.class)
 public class ListWrapperTestCase
