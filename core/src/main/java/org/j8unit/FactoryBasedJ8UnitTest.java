2b056e8e2a28f3c0fe120931bdbc6ff9bdb4210f
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-10-31 00:04:58 +0100

    Javadoc++ (FactoryBasedJ8UnitTest)

--- core/src/main/java/org/j8unit/FactoryBasedJ8UnitTest.java
+++ core/src/main/java/org/j8unit/FactoryBasedJ8UnitTest.java
@@ -1,24 +1,55 @@
 package org.j8unit;
 
 import java.util.function.Supplier;
-import org.junit.Before;
 
 /**
- * Any FactoryBasedJ8UnitTest implements this interface; in result, all those tests provide
- * {@linkplain #getSUTFactory() a factory method} to query a fresh subject-under-test instance. This behaviour is
- * somehow similar to JUnit's {@link Before} annotated methods (plus its typical usage), whereas within an interface
- * there is no class' member that could be refreshed before each test. Thus, this is the way it works.
+ * <p>
+ * In case a subject-under-test (SUT) based J8Unit test uses a SUT factory to produce fresh SUT instances, this
+ * interface should be used preference to {@link J8UnitTest}. In detail, this {@code interface} specifies
+ * {@linkplain #getSUTFactory() a factory query method} that is used within the {@code default} implementation of
+ * {@link #createNewSUT()}.
+ * </p>
+ *
+ * @see J8UnitTest
  *
  * @param <SUT>
  *            the type of the subject-under-test
- * @author <a href="mailto:stefan.gasterstaedt@informaticum.de">Stefan Gasterst&auml;dt</a>
+ * @since 4.12
  */
 @FunctionalInterface
 public abstract interface FactoryBasedJ8UnitTest<SUT>
 extends J8UnitTest<SUT> {
 
+    /**
+     * <p>
+     * Returns a factory to be used when {@linkplain #createNewSUT() creating new subject-under-test instances} (each of
+     * type {@code SUT}).
+     * </p>
+     *
+     * @return a SUT factory
+     */
     public abstract Supplier<SUT> getSUTFactory();
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>
+     * Due to the {@linkplain #getSUTFactory() SUT factory based} nature of this J8Unit test, this method has been
+     * implemented with the following {@code default} behaviour:
+     * </p>
+     *
+     * <pre>
+     * {@code
+     * {
+     * final Supplier<SUT> factory = this.getSUTFactory();
+     * assert factory != null;
+     * final SUT sut = factory.get();
+     * assert sut != null;
+     * return sut;
+     * }
+     * }
+     * </pre>
+     */
     @Override
     public default SUT createNewSUT() {
         final Supplier<SUT> factory = this.getSUTFactory();
