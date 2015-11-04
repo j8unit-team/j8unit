09b59ea990b51061e4a94c7088b9a7fe319e0e40
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-11-04 21:54:15 +0100

    JavaDoc++

--- repository/src/test/java/org/j8unit/repository/java/JavaBug.java
+++ repository/src/test/java/org/j8unit/repository/java/JavaBug.java
@@ -1,8 +1,8 @@
 package org.j8unit.repository.java;
 
 /**
- * Marker interface to tag Java bugs enabling JUnit exclusion. If, otherwise, not excluded the maven build will abort by
- * failing tests.
+ * Marker interface to tag Java bugs (enabling JUnit exclusion). If, otherwise, not excluded the maven build will abort
+ * by failing tests.
  */
 public abstract interface JavaBug {
 }
