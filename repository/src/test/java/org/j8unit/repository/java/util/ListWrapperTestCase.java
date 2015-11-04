22e1589ebcc9e308b82cce2fcf2df39e3784e842
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-11-04 21:28:34 +0100

    Ver-Category-sieren der bekannten Java-Bugs in den java.util-Listem

--- repository/src/test/java/org/j8unit/repository/java/util/ListWrapperTestCase.java
+++ repository/src/test/java/org/j8unit/repository/java/util/ListWrapperTestCase.java
@@ -7,18 +7,19 @@ import static java.util.Collections.unmodifiableList;
 import java.util.List;
 import java.util.function.Supplier;
 import org.j8unit.FactoryBasedJ8UnitTest;
+import org.j8unit.repository.java.JavaBug;
 import org.j8unit.repository.java.lang.ObjectTests;
 import org.j8unit.runners.J8Parameterized;
 import org.j8unit.runners.parameterized.J8BlockJUnit4ClassRunnerWithParametersFactory;
-import org.junit.Ignore;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 import org.junit.runners.Parameterized.UseParametersRunnerFactory;
 
-@Ignore("Java has some bugs ;-(")
 @RunWith(J8Parameterized.class)
 @UseParametersRunnerFactory(J8BlockJUnit4ClassRunnerWithParametersFactory.class)
+@Category(JavaBug.class)
 public class ListWrapperTestCase
 implements ObjectTests<List<Object>>, UnmodifiableListTests<List<Object>, Object>, FactoryBasedJ8UnitTest<List<Object>> {
 
