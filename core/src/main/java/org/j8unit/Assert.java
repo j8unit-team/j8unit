1edd65ceebfd5116a73b1c4a4a07f2ea3c0d19c3
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-11-07 21:06:00 +0100

    Erste Implementierungen für Supplier-Methoden

--- core/src/main/java/org/j8unit/Assert.java
+++ core/src/main/java/org/j8unit/Assert.java
@@ -9,31 +9,33 @@ extends org.junit.Assert {
     protected Assert() {
     }
 
-    static public void assertTrue(final Supplier<String> message, final boolean condition) {
-        assertTrue(message == null ? null : message.get(), condition);
+    public static void assertTrue(final Supplier<String> message, final boolean condition) {
+        if (!condition) {
+            fail(message);
+        }
     }
 
-    static public void assertFalse(final Supplier<String> message, final boolean condition) {
-        assertFalse(message == null ? null : message.get(), condition);
+    public static void assertFalse(final Supplier<String> message, final boolean condition) {
+        assertTrue(message, !condition);
     }
 
-    static public void fail(final Supplier<String> message) {
+    public static void fail(final Supplier<String> message) {
         fail(message == null ? null : message.get());
     }
 
-    static public void assertEquals(final Supplier<String> message, final Object expected, final Object actual) {
+    public static void assertEquals(final Supplier<String> message, final Object expected, final Object actual) {
         assertEquals(message == null ? null : message.get(), expected, actual);
     }
 
-    static public void assertNotEquals(final Supplier<String> message, final Object unexpected, final Object actual) {
+    public static void assertNotEquals(final Supplier<String> message, final Object unexpected, final Object actual) {
         assertNotEquals(message == null ? null : message.get(), unexpected, actual);
     }
 
-    static public void assertNotEquals(final Supplier<String> message, final long unexpected, final long actual) {
+    public static void assertNotEquals(final Supplier<String> message, final long unexpected, final long actual) {
         assertNotEquals(message == null ? null : message.get(), unexpected, actual);
     }
 
-    static public void assertNotEquals(final Supplier<String> message, final double unexpected, final double actual, final double delta) {
+    public static void assertNotEquals(final Supplier<String> message, final double unexpected, final double actual, final double delta) {
         assertNotEquals(message == null ? null : message.get(), unexpected, actual, delta);
     }
 
@@ -82,40 +84,40 @@ extends org.junit.Assert {
         assertArrayEquals(message == null ? null : message.get(), expecteds, actuals, delta);
     }
 
-    static public void assertEquals(final Supplier<String> message, final double expected, final double actual, final double delta) {
+    public static void assertEquals(final Supplier<String> message, final double expected, final double actual, final double delta) {
         assertEquals(message == null ? null : message.get(), expected, actual, delta);
     }
 
-    static public void assertEquals(final Supplier<String> message, final float expected, final float actual, final float delta) {
+    public static void assertEquals(final Supplier<String> message, final float expected, final float actual, final float delta) {
         assertEquals(message == null ? null : message.get(), expected, actual, delta);
     }
 
-    static public void assertNotEquals(final Supplier<String> message, final float unexpected, final float actual, final float delta) {
+    public static void assertNotEquals(final Supplier<String> message, final float unexpected, final float actual, final float delta) {
         assertNotEquals(message == null ? null : message.get(), unexpected, actual, delta);
     }
 
-    static public void assertEquals(final Supplier<String> message, final long expected, final long actual) {
+    public static void assertEquals(final Supplier<String> message, final long expected, final long actual) {
         assertEquals(message == null ? null : message.get(), expected, actual);
     }
 
     @Deprecated
-    static public void assertEquals(final Supplier<String> message, final double expected, final double actual) {
+    public static void assertEquals(final Supplier<String> message, final double expected, final double actual) {
         assertEquals(message == null ? null : message.get(), expected, actual);
     }
 
-    static public void assertNotNull(final Supplier<String> message, final Object object) {
+    public static void assertNotNull(final Supplier<String> message, final Object object) {
         assertNotNull(message == null ? null : message.get(), object);
     }
 
-    static public void assertNull(final Supplier<String> message, final Object object) {
+    public static void assertNull(final Supplier<String> message, final Object object) {
         assertNull(message == null ? null : message.get(), object);
     }
 
-    static public void assertSame(final Supplier<String> message, final Object expected, final Object actual) {
+    public static void assertSame(final Supplier<String> message, final Object expected, final Object actual) {
         assertSame(message == null ? null : message.get(), expected, actual);
     }
 
-    static public void assertNotSame(final Supplier<String> message, final Object unexpected, final Object actual) {
+    public static void assertNotSame(final Supplier<String> message, final Object unexpected, final Object actual) {
         assertNotSame(message == null ? null : message.get(), unexpected, actual);
     }
 
