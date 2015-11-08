93506f3167831cd23317306834096c32e4041993
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-11-08 18:47:02 +0100

    Assert nun auch mit CharSequence-basierten Assertion-Methoden

--- core/src/main/java/org/j8unit/Assert.java
+++ core/src/main/java/org/j8unit/Assert.java
@@ -1,11 +1,23 @@
 package org.j8unit;
 
+import java.util.Objects;
 import java.util.function.Supplier;
 import org.junit.internal.ArrayComparisonFailure;
 
 /**
- * In addition to the JUnit's {@link org.junit.Assert} this Assert extension provides {@link Supplier}-based assertion
- * methods.
+ * <p>
+ * In addition to the JUnit's {@link org.junit.Assert} this extension provides {@link Supplier}-based assertion methods.
+ * In result, it is possible to defer the message creation until required (or even skip creation unless necessary).
+ *
+ * Further, the message may be provided as a more general {@link CharSequence}. Similar to the aforementioned methods
+ * the {@link CharSequence}-based assertion methods give you more flexibility and allow deferred message creation.
+ * </p>
+ *
+ * <p>
+ * To be honest, all the methods currently just delegate to the origin {@link String}-based methods of
+ * {@linkplain org.junit.Assert the JUnit Assert class}. Nevertheless, you can use them by now; The next version will
+ * contain smart implementations too. Promised.
+ * </p>
  */
 public class Assert
 extends org.junit.Assert {
@@ -13,14 +25,127 @@ extends org.junit.Assert {
     protected Assert() {
     }
 
+    public static void assertTrue(final CharSequence message, final boolean condition) {
+        assertTrue(Objects.toString(message, null), condition);
+    }
+
+    public static void assertFalse(final CharSequence message, final boolean condition) {
+        assertFalse(Objects.toString(message, null), condition);
+    }
+
+    public static void fail(final CharSequence message) {
+        fail(Objects.toString(message, null));
+    }
+
+    public static void assertEquals(final CharSequence message, final Object expected, final Object actual) {
+        assertEquals(Objects.toString(message, null), expected, actual);
+    }
+
+    public static void assertNotEquals(final CharSequence message, final Object unexpected, final Object actual) {
+        assertNotEquals(Objects.toString(message, null), unexpected, actual);
+    }
+
+    public static void assertNotEquals(final CharSequence message, final long unexpected, final long actual) {
+        assertNotEquals(Objects.toString(message, null), unexpected, actual);
+    }
+
+    public static void assertNotEquals(final CharSequence message, final double unexpected, final double actual, final double delta) {
+        assertNotEquals(Objects.toString(message, null), unexpected, actual, delta);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final boolean[] expecteds, final boolean[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final byte[] expecteds, final byte[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final char[] expecteds, final char[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final short[] expecteds, final short[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final int[] expecteds, final int[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final long[] expecteds, final long[] actuals)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final double[] expecteds, final double[] actuals, final double delta)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals, delta);
+    }
+
+    public static void assertArrayEquals(final CharSequence message, final float[] expecteds, final float[] actuals, final float delta)
+    throws ArrayComparisonFailure {
+        assertArrayEquals(Objects.toString(message, null), expecteds, actuals, delta);
+    }
+
+    public static void assertEquals(final CharSequence message, final double expected, final double actual, final double delta) {
+        assertEquals(Objects.toString(message, null), expected, actual, delta);
+    }
+
+    public static void assertEquals(final CharSequence message, final float expected, final float actual, final float delta) {
+        assertEquals(Objects.toString(message, null), expected, actual, delta);
+    }
+
+    public static void assertNotEquals(final CharSequence message, final float unexpected, final float actual, final float delta) {
+        assertNotEquals(Objects.toString(message, null), unexpected, actual, delta);
+    }
+
+    public static void assertEquals(final CharSequence message, final long expected, final long actual) {
+        assertEquals(Objects.toString(message, null), expected, actual);
+    }
+
+    @Deprecated
+    public static void assertEquals(final CharSequence message, final double expected, final double actual) {
+        assertEquals(Objects.toString(message, null), expected, actual);
+    }
+
+    public static void assertNotNull(final CharSequence message, final Object object) {
+        assertNotNull(Objects.toString(message, null), object);
+    }
+
+    public static void assertNull(final CharSequence message, final Object object) {
+        assertNull(Objects.toString(message, null), object);
+    }
+
+    public static void assertSame(final CharSequence message, final Object expected, final Object actual) {
+        assertSame(Objects.toString(message, null), expected, actual);
+    }
+
+    public static void assertNotSame(final CharSequence message, final Object unexpected, final Object actual) {
+        assertNotSame(Objects.toString(message, null), unexpected, actual);
+    }
+
+    @Deprecated
+    public static void assertEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals) {
+        assertEquals(Objects.toString(message, null), expecteds, actuals);
+    }
+
     public static void assertTrue(final Supplier<String> message, final boolean condition) {
-        if (!condition) {
-            fail(message);
-        }
+        assertTrue(message == null ? null : message.get(), condition);
     }
 
     public static void assertFalse(final Supplier<String> message, final boolean condition) {
-        assertTrue(message, !condition);
+        assertFalse(message == null ? null : message.get(), condition);
     }
 
     public static void fail(final Supplier<String> message) {
