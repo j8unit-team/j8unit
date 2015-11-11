cc365959a5d1d4d28812643b3448811497400377
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-11-11 21:36:25 +0100

    Package-Info für J8Unit-Spring

---
+++ spring/src/main/java/org/j8unit/spring/runners/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * According to the goals of <em><a href="j8unit.org">J8Unit (Spring)</a></em>, this packages contains any required
+ * extension of {@code org.j8unit.spring.runners}' classes.
+ * </p>
+ *
+ * @author <a href="mailto:sascha.zak@zak-digital.de">Sascha Zak</a>
+ *
+ * @since 4.1.7
+ */
+package org.j8unit.spring.runners;
