291d38ca8cb0e0ae80f905c601e718695296f83c
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-12-04 12:13:33 +0100

    Renaming

--- repository/src/main/java/org/j8unit/repository/util/TestDataUtil.java
+++ repository/src/main/java/org/j8unit/repository/util/TestDataUtil.java
@@ -5,7 +5,7 @@ import static java.util.stream.Collectors.toList;
 import java.util.List;
 import java.util.function.Function;
 
-public enum TestData {
+public enum TestDataUtil {
     ;
 
     public static final Function<Object, Object[]> ENVELOPE = x -> new Object[] { x };
