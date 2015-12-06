d082d2150b3ae93ad4b518cbd4d2629a84da084d
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-12-06 15:55:26 +0100

    Move TestDataUtil to J8Unit Repository as a meanwhile solution

--- repository/src/main/java/org/j8unit/repository/TestDataUtil.java
+++ repository/src/main/java/org/j8unit/repository/TestDataUtil.java
@@ -1,22 +1,29 @@
-package org.j8unit;
+package org.j8unit.repository;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 import java.util.List;
 import java.util.function.Function;
 
+/**
+ * TODO: Move to J8Unit Core Project.
+ *
+ * @deprecated Will be migrated to J8Unit Core Project within its next release.
+ */
+@Deprecated
 public enum TestDataUtil {
     ;
 
     public static final Function<Object, Object[]> ENVELOPE = x -> new Object[] { x };
 
-    public static final <T> List<Object[]> testDataOf(final T[] values) {
+    @SafeVarargs
+    public static final <T> List<Object[]> testDataOfSingletons(final T... values) {
         return stream(values).map(ENVELOPE).collect(toList());
     }
 
     public static final <E extends Enum<E>> List<Object[]> testDataOf(final Class<E> enumClass) {
         assert enumClass.isEnum();
-        return testDataOf(enumClass.getEnumConstants());
+        return testDataOfSingletons(enumClass.getEnumConstants());
     }
 
 }
