2d19d78b0b94aa121144637d6581349b8c1edfff
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2015-12-24 12:14:05 +0100

    New: Testing the core types

---
+++ core/src/test/java/org/j8unit/J8UnitTestTest.java
@@ -0,0 +1,16 @@
+package org.j8unit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+
+public class J8UnitTestTest {
+
+    @Test
+    public void testFunctionalInterfaceStyle() {
+        final J8UnitTest<String> t = String::new;
+        final String sut = t.createNewSUT();
+        assertNotNull(sut);
+        assertEquals("", sut);
+    }
+}
