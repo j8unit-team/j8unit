47d7cda3cad082d324d4f5b3f9dc57fea8cfe4fa
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2016-01-07 21:01:39 +0100

    Remove stupid tests

--- repository/src/main/java/org/j8unit/repository/RepositoryClassTests.java
+++ repository/src/main/java/org/j8unit/repository/RepositoryClassTests.java
@@ -1,37 +1,7 @@
 package org.j8unit.repository;
 
 import org.j8unit.J8UnitTest;
-import org.junit.Test;
 
 public interface RepositoryClassTests<SUT>
 extends J8UnitTest<Class<SUT>> {
-
-    @Test
-    default void testAnnotationess()
-    throws Exception {
-        final Class<SUT> sut = createNewSUT();
-        assert sut != null;
-    }
-
-    @Test
-    public default void testEnumness()
-    throws Exception {
-        final Class<SUT> sut = createNewSUT();
-        assert sut != null;
-    }
-
-    @Test
-    default void testInterfaceness()
-    throws Exception {
-        final Class<SUT> sut = createNewSUT();
-        assert sut != null;
-    }
-
-    @Test
-    default void testPrimitiveness()
-    throws Exception {
-        final Class<SUT> sut = createNewSUT();
-        assert sut != null;
-    }
-
 }
