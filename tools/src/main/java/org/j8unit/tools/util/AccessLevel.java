3e3f679529c85623f9c601174fcfdc5f50b77e74
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2016-01-29 09:23:05 +0100

    State-of-the-Art: enum AccessLevel

--- tools/src/main/java/org/j8unit/tools/util/AccessLevel.java
+++ tools/src/main/java/org/j8unit/tools/util/AccessLevel.java
@@ -1,32 +1,39 @@
 package org.j8unit.tools.util;
 
 import static java.util.Objects.requireNonNull;
+import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.reflect.Member;
 import java.lang.reflect.Modifier;
 
 /**
- * Enumeration of the Java access levels. Beside the obvious {@link #PUBLIC public}, {@link #PROTECTED protected}, and
- * {@link #PRIVATE private} there is an explicit element representing the modifier-less {@link #PACKAGE_PRIVATE package
- * private} case.
+ * <p>
+ * Enumeration of the Java access levels. In addition to the obvious {@link #PUBLIC public}, {@link #PROTECTED
+ * protected}, and {@link #PRIVATE private} access there is an explicit element representing the modifier-less
+ * {@link #PACKAGE_PRIVATE package private} case.
+ * </p>
  *
- * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html"> The Java&trade; Tutorials:
+ * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html">The Java&trade; Tutorials:
  *      Controlling Access to Members of a Class</a>
- *
- * @author Stefan Gasterst&auml;dt (SGA)
  */
 public enum AccessLevel {
 
     /**
+     * <p>
      * Enumeration constant representing {@code public} access.
+     * </p>
      */
-    PUBLIC {
+    PUBLIC(Lookup.PUBLIC) {
 
         /**
-         * Returns {@code true} if the modifier represents a {@code public} element. Returns {@code false} otherwise.
+         * <p>
+         * Returns {@code true} if the {@code modifier} represents a {@code public} element. Returns {@code false}
+         * otherwise.
+         * </p>
          *
          * @see Modifier#isPublic(int)
          *
-         * @return {@code true} if the given modifier represents a {@code public} element; {@code false} otherwise
+         * @return {@code true} if the given {@code modifier} represents a {@code public} element; {@code false}
+         *         otherwise
          */
         @Override
         public boolean matches(final int modifier) {
@@ -36,16 +43,22 @@ public enum AccessLevel {
     },
 
     /**
+     * <p>
      * Enumeration constant representing {@code protected} access.
+     * </p>
      */
-    PROTECTED {
+    PROTECTED(Lookup.PROTECTED) {
 
         /**
-         * Returns {@code true} if the modifier represents a {@code protected} element. Returns {@code false} otherwise.
+         * <p>
+         * Returns {@code true} if the {@code modifier} represents a {@code protected} element. Returns {@code false}
+         * otherwise.
+         * </p>
          *
          * @see Modifier#isProtected(int)
          *
-         * @return {@code true} if the given modifier represents a {@code protected} element; {@code false} otherwise
+         * @return {@code true} if the given {@code modifier} represents a {@code protected} element; {@code false}
+         *         otherwise
          */
         @Override
         public boolean matches(final int modifier) {
@@ -55,16 +68,22 @@ public enum AccessLevel {
     },
 
     /**
+     * <p>
      * Enumeration constant representing {@code private} access.
+     * </p>
      */
-    PRIVATE {
+    PRIVATE(Lookup.PRIVATE) {
 
         /**
-         * Returns {@code true} if the modifier represents a {@code private} element. Returns {@code false} otherwise.
+         * <p>
+         * Returns {@code true} if the {@code modifier} represents a {@code private} element. Returns {@code false}
+         * otherwise.
+         * </p>
          *
          * @see Modifier#isPrivate(int)
          *
-         * @return {@code true} if the given modifier represents a {@code private} element; {@code false} otherwise
+         * @return {@code true} if the given {@code modifier} represents a {@code private} element; {@code false}
+         *         otherwise
          */
         @Override
         public boolean matches(final int modifier) {
@@ -74,16 +93,20 @@ public enum AccessLevel {
     },
 
     /**
-     * Enumeration constant representing <em>package private</em> access.
+     * <p>
+     * Enumeration constant representing <em>package private</em> access (aka. default access).
+     * </p>
      */
-    PACKAGE_PRIVATE {
+    PACKAGE_PRIVATE(Lookup.PACKAGE) {
 
         /**
-         * Returns {@code true} if the modifier represents a <em>package private</em> element. Returns {@code false}
-         * otherwise.
+         * <p>
+         * Returns {@code true} if the {@code modifier} represents a <em>package private</em> element. Returns
+         * {@code false} otherwise.
+         * </p>
          *
-         * @return {@code true} if the given modifier represents a <em>package private</em> element; {@code false}
-         *         otherwise
+         * @return {@code true} if the given {@code modifier} represents a <em>package private</em> element;
+         *         {@code false} otherwise
          */
         @Override
         public boolean matches(final int modifier) {
@@ -92,8 +115,22 @@ public enum AccessLevel {
 
     };
 
+    // TODO: What can we do with the modifier information? Most probably, there are fancy ideas out there ...
+    @SuppressWarnings("unused")
+    private final int modifier;
+
     /**
-     * Predicate method matching any modifier that fits to the specific access level.
+     * @param modifier
+     *            a single-bit mask representing the access level
+     */
+    private AccessLevel(final int modifier) {
+        this.modifier = modifier;
+    }
+
+    /**
+     * <p>
+     * Predicate-method matching any {@code modifier} that fits to {@code this} specific access level.
+     * </p>
      *
      * @see #PUBLIC
      * @see #PROTECTED
@@ -102,65 +139,71 @@ public enum AccessLevel {
      *
      * @param modifier
      *            the modifier to use for matching query
-     * @return {@code true} if the given modifier fits to the specific access level; {@code false} otherwise
+     * @return {@code true} if the given {@code modifier} fits to {@code this} specific access level; {@code false}
+     *         otherwise
      */
     public abstract boolean matches(int modifier);
 
     /**
-     * Predicate method matching any modifier that does not hold {@link #matches(int)}.
+     * <p>
+     * Predicate-method matching any {@code modifier} that does not hold {@link #matches(int)}.
+     * </p>
      *
      * @see #matches(int)
      *
      * @param modifier
      *            the modifier to use for mismatching query
-     * @return {@code true} if the given modifier does not hold {@link #matches(int)}; {@code false} otherwise
+     * @return {@code true} if the given {@code modifier} does not hold {@link #matches(int)}; {@code false} otherwise
      */
     public final boolean mismatches(final int modifier) {
         return !this.matches(modifier);
     }
 
     /**
-     * Predicate method matching any {@linkplain Class class} that (using its {@linkplain Class#getModifiers()
-     * modifiers}) holds {@link #matches(int)}.
+     * <p>
+     * Predicate-method matching any {@link Class entity} that &ndash;&nbsp;using its {@linkplain Class#getModifiers()
+     * modifiers}&nbsp;&ndash; holds {@link #matches(int)}.
+     * </p>
      *
      * @see #matches(int)
      *
-     * @param clazz
-     *            the class to use for matching query
-     * @return {@code true} if the class' {@linkplain Class#getModifiers() modifiers} hold {@link #matches(int)};
-     *         {@code false} otherwise
+     * @param entity
+     *            the entity to use for matching query
+     * @return {@code true} if the {@code entity}'s modifiers hold {@link #matches(int)}; {@code false} otherwise
      */
-    public final boolean matches(final Class<?> clazz) {
-        requireNonNull(clazz);
-        return this.matches(clazz.getModifiers());
+    public final boolean matches(final Class<?> entity) {
+        requireNonNull(entity);
+        return this.matches(entity.getModifiers());
     }
 
     /**
-     * Predicate method matching any {@linkplain Class class} that (using its {@linkplain Class#getModifiers()
-     * modifiers}) holds {@link #mismatches(int)}.
+     * <p>
+     * Predicate-method matching any {@link Class entity} that &ndash;&nbsp;using its {@linkplain Class#getModifiers()
+     * modifiers}&nbsp;&ndash; holds {@link #mismatches(int)}.
+     * </p>
      *
      * @see #mismatches(int)
      *
-     * @param clazz
-     *            the class to use for mismatching query
-     * @return {@code true} if the class' {@linkplain Class#getModifiers() modifiers} hold {@link #mismatches(int)};
-     *         {@code false} otherwise
+     * @param entity
+     *            the entity to use for mismatching query
+     * @return {@code true} if the {@code entity}'s modifiers hold {@link #mismatches(int)}; {@code false} otherwise
      */
-    public final boolean mismatches(final Class<?> clazz) {
-        requireNonNull(clazz);
-        return this.mismatches(clazz.getModifiers());
+    public final boolean mismatches(final Class<?> entity) {
+        requireNonNull(entity);
+        return this.mismatches(entity.getModifiers());
     }
 
     /**
-     * Predicate method matching any {@linkplain Member member} that (using its {@linkplain Class#getModifiers()
-     * modifiers}) holds {@link #matches(int)}.
+     * <p>
+     * Predicate-method matching any {@link Member member} that &ndash;&nbsp;using its {@linkplain Member#getModifiers()
+     * modifiers}&nbsp;&ndash; holds {@link #matches(int)}.
+     * </p>
      *
      * @see #matches(int)
      *
      * @param member
      *            the member to use for matching query
-     * @return {@code true} if the member's {@linkplain Member#getModifiers() modifiers} hold {@link #matches(int)};
-     *         {@code false} otherwise
+     * @return {@code true} if the {@code member}'s modifiers hold {@link #matches(int)}; {@code false} otherwise
      */
     public final boolean matches(final Member member) {
         requireNonNull(member);
@@ -168,15 +211,16 @@ public enum AccessLevel {
     }
 
     /**
-     * Predicate method matching any {@linkplain Member member} that (using its {@linkplain Member#getModifiers()
-     * modifiers} ) holds {@link #mismatches(int)}.
+     * <p>
+     * Predicate-method matching any {@link Member member} that &ndash;&nbsp;using its {@linkplain Member#getModifiers()
+     * modifiers}&nbsp;&ndash; holds {@link #mismatches(int)}.
+     * </p>
      *
      * @see #mismatches(int)
      *
      * @param clazz
      *            the member to use for mismatching query
-     * @return {@code true} if the member's {@linkplain Member#getModifiers() modifiers} hold {@link #mismatches(int)};
-     *         {@code false} otherwise
+     * @return {@code true} if the {@code member}'s modifiers hold {@link #mismatches(int)}; {@code false} otherwise
      */
     public final boolean mismatches(final Member member) {
         requireNonNull(member);
