2b702836b5cb57fc6f901775e57edf53aab9d287
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2016-02-09 01:07:09 +0100

    State-of-the-Art: utility enum TypeKind

--- tools/src/main/java/org/j8unit/tools/util/TypeKind.java
+++ tools/src/main/java/org/j8unit/tools/util/TypeKind.java
@@ -1,83 +1,163 @@
 package org.j8unit.tools.util;
 
 import static java.util.Arrays.stream;
+import static java.util.Objects.requireNonNull;
 import java.lang.reflect.Modifier;
 import java.util.Optional;
 
 /**
- * {@linkplain Class#getEnclosingClass() There are five kinds of classes (or interfaces):}
+ * <p>
+ * Enumeration of the kinds of Java types; Actually, there are five kinds:
+ * </p>
+ *
  * <ul>
- * <li>Top level classes</li>
- * <li>Nested classes (static member classes)</li>
- * <li>Inner classes (non-static member classes)</li>
- * <li>Local classes (named classes declared within a method)</li>
- * <li>Anonymous classes</li>
+ * <li>Top level types ({@link #TOP_LEVEL})</li>
+ * <li>Nested types (static member types) ({@link #NESTED})</li>
+ * <li>Inner types (non-static member types) ({@link #INNER})</li>
+ * <li>Local types (named types declared within a method) ({@link #LOCAL})</li>
+ * <li>Anonymous types ({@link #ANONYMOUS})</li>
  * </ul>
- * 
+ *
+ * @see Class#getEnclosingClass() source code of {@code Class#getEnclosingClass()}
  * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/nested.html"> The Java&trade; Tutorials: Nested
  *      Classes</a>
  * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/localclasses.html"> The Java&trade; Tutorials:
  *      Local Classes</a>
  * @see <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/anonymousclasses.html"> The Java&trade; Tutorials:
  *      Anonymous Classes</a>
- *
- * @author Stefan Gasterst&auml;dt (SGA)
  */
 public enum TypeKind {
 
+    /**
+     * <p>
+     * Enumeration constant representing top level types; that is, all types without an enclosing type.
+     * </p>
+     *
+     * @see Class#getEnclosingClass()
+     */
     TOP_LEVEL {
 
         @Override
-        public boolean matches(final Class<?> clazz) {
-            return clazz.getEnclosingClass() == null;
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return type.getEnclosingClass() == null;
         }
 
     },
 
+    /**
+     * <p>
+     * Enumeration constant representing nested types; that is, all static member types.
+     * </p>
+     *
+     * @see Class#isMemberClass()
+     * @see Modifier#isStatic(int)
+     */
     NESTED {
 
         @Override
-        public boolean matches(final Class<?> clazz) {
-            return clazz.isMemberClass() && Modifier.isStatic(clazz.getModifiers());
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return type.isMemberClass() && Modifier.isStatic(type.getModifiers());
         }
 
     },
 
+    /**
+     * <p>
+     * Enumeration constant representing inner types; that is, all non-static member types.
+     * </p>
+     *
+     * @see Class#isMemberClass()
+     * @see Modifier#isStatic(int)
+     */
     INNER {
 
         @Override
-        public boolean matches(final Class<?> clazz) {
-            return clazz.isMemberClass() && !Modifier.isStatic(clazz.getModifiers());
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return type.isMemberClass() && !Modifier.isStatic(type.getModifiers());
         }
 
     },
 
+    /**
+     * <p>
+     * Enumeration constant representing local types; that is, all named types declared within a method.
+     * </p>
+     *
+     * @see Class#isLocalClass()
+     */
     LOCAL {
 
         @Override
-        public boolean matches(final Class<?> clazz) {
-            return clazz.isLocalClass();
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return type.isLocalClass();
         }
 
     },
 
+    /**
+     * <p>
+     * Enumeration constant representing anonymous types.
+     * </p>
+     *
+     * @see Class#isAnonymousClass()
+     */
     ANONYMOUS {
 
         @Override
-        public boolean matches(final Class<?> clazz) {
-            return clazz.isAnonymousClass();
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return type.isAnonymousClass();
         }
 
     };
 
-    public abstract boolean matches(final Class<?> clazz);
+    /**
+     * <p>
+     * Predicate method matching any {@link Class type} that fits to the specific kind of Java type.
+     * </p>
+     *
+     * @see #TOP_LEVEL
+     * @see #NESTED
+     * @see #INNER
+     * @see #LOCAL
+     * @see #ANONYMOUS
+     *
+     * @param modifier
+     *            the Java type to use for matching query
+     * @return {@code true} if the given modifier fits to the specific kind of Java type; {@code false} otherwise
+     */
+    public abstract boolean matches(final Class<?> type);
 
-    public final boolean mismatches(final Class<?> clazz) {
-        return !this.matches(clazz);
+    /**
+     * <p>
+     * Predicate method matching any {@linkplain Class type} that does not hold {@link #matches(int)}.
+     * </p>
+     *
+     * @see #matches(int)
+     *
+     * @param type
+     *            the Java type to use for mismatching query
+     * @return {@code true} if the given modifier does not hold {@link #matches(Class)}; {@code false} otherwise
+     */
+    public final boolean mismatches(final Class<?> type) {
+        requireNonNull(type);
+        return !this.matches(type);
     }
 
-    public static final TypeKind kindOf(final Class<?> clazz) {
-        final Optional<TypeKind> kind = stream(TypeKind.values()).filter(k -> k.matches(clazz)).findFirst();
+    /**
+     * Returns the {@link TypeKind} {@code enum} constant according to the query Java {@code type}.
+     *
+     * @param type
+     *            the query Java type
+     * @return the {@code TypeKind} {@code enum} constant according to the query Java {@code type}
+     */
+    public static final TypeKind kindOf(final Class<?> type) {
+        requireNonNull(type);
+        final Optional<TypeKind> kind = stream(TypeKind.values()).filter(k -> k.matches(type)).findFirst();
         assert kind.isPresent() : "A class cannot have a kind different to top-level, nested, inner, local, or anonymous!";
         return kind.get();
     }
