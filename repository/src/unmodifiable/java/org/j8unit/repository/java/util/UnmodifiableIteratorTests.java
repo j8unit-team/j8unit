d137e889a147afa5f376d42d6ce719c50ef9f9ec
Stefan Gasterstädt <stefan.gasterstaedt@informaticum.de>
2016-02-15 23:10:27 +0100

    Unmodifiable Iterable/Iterator/Collection/List/

---
+++ repository/src/unmodifiable/java/org/j8unit/repository/java/util/UnmodifiableIteratorTests.java
@@ -0,0 +1,24 @@
+package org.j8unit.repository.java.util;
+
+import static org.junit.Assume.assumeTrue;
+import java.util.Iterator;
+import org.j8unit.repository.categories.J8UnitRepository;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@FunctionalInterface
+@Category(J8UnitRepository.class)
+public interface UnmodifiableIteratorTests<SUT extends Iterator<E>, E>
+extends IteratorTests<SUT, E> {
+
+    @Test(expected = UnsupportedOperationException.class)
+    public default void removeMustThrowUOE()
+    throws Exception {
+        final SUT sut = createNewSUT();
+        assert sut != null;
+        assumeTrue("The given Iterable is empty; Thus, this test becomes useless.", sut.hasNext());
+        sut.next();
+        sut.remove();
+    }
+
+}
