d80b3ea4d8d67e6fe65fb7c61e96e751a9e5c168
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-08 22:55:02 +0100

    Fully implement the TypeNature detection

--- tools/src/main/java/org/j8unit/generator/analysis/TypeNatures.java
+++ tools/src/main/java/org/j8unit/generator/analysis/TypeNatures.java
@@ -1,12 +1,18 @@
 package org.j8unit.generator.analysis;
 
-import static java.util.Arrays.asList;
+import static java.util.Arrays.stream;
 import static java.util.Collections.unmodifiableSet;
 import static java.util.Objects.requireNonNull;
 import static org.j8unit.generator.util.Sets.asSet;
-import java.util.List;
+import static org.j8unit.generator.util.TypeAnalysis.calculateNearestMatchingParents;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import org.j8unit.generator.api.CustomWarnings;
+import org.j8unit.generator.util.Mismatcher;
 
 /**
  * <p>
@@ -14,7 +20,7 @@ import org.j8unit.generator.api.CustomWarnings;
  *
  * Java types can be {@linkplain #NON_GENERIC pure} (simple types without type arguments), they can be
  * {@linkplain #GENERIC generic} (types with type argument(s)), or they can be {@linkplain #RAW raw} (generic types but
- * without the according type argument(s)).
+ * without the according type argument(s)). (<em>Note, the RAW type nature is inherited by sub-types!</em>)
  * <p>
  *
  * <p>
@@ -30,12 +36,20 @@ import org.j8unit.generator.api.CustomWarnings;
  *      Types</a>
  */
 public enum TypeNatures
-implements CustomWarnings {
+implements Mismatcher<Class<?>>, CustomWarnings {
 
     /**
      * Enumeration constant representing non-generic (pure) types.
      */
-    NON_GENERIC,
+    NON_GENERIC {
+
+        @Override
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return (type.getTypeParameters().length == 0) && RAW.mismatches(type);
+        }
+
+    },
 
     /**
      * Enumeration constant representing generic types that do have according type arguments.
@@ -43,16 +57,44 @@ implements CustomWarnings {
      * @see <a href="https://docs.oracle.com/javase/tutorial/java/generics/types.html">The Java&trade; Tutorials:
      *      Generic Types</a>
      */
-    GENERIC,
+    GENERIC {
+
+        @Override
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            return (type.getTypeParameters().length != 0) && RAW.mismatches(type);
+        }
+
+    },
 
     /**
      * Enumeration constant representing raw types, i.&thinsp;e., generic classes or interfaces but without type
      * arguments.
      *
+     * Note, any sub-type of a {@code RAW} type is considered to be {@code RAW} as well.
+     *
      * @see <a href="https://docs.oracle.com/javase/tutorial/java/generics/rawTypes.html">The Java&trade; Tutorials: Raw
      *      Types</a>
      */
-    RAW("rawtypes");
+    RAW("rawtypes") {
+
+        @Override
+        public boolean matches(final Class<?> type) {
+            requireNonNull(type);
+            for (final Entry<Class<?>, ? extends Type> parent : calculateNearestMatchingParents(type, Objects::nonNull).entrySet()) {
+                // Case A) parent type's parameters are not considered
+                if ((parent.getKey().getTypeParameters().length != 0) && !(parent.getValue() instanceof ParameterizedType)) {
+                    return true;
+                }
+                // Case B) RAW type nature is inherited
+                if (this.matches(parent.getKey())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    };
 
     private final Set<String> warnings;
 
@@ -74,39 +116,9 @@ implements CustomWarnings {
      */
     public static final TypeNatures natureOf(final Class<?> type) {
         requireNonNull(type);
-        if (JAVA8_RAW_GENERIC_TYPES.contains(type)) {
-            // TODO: Identify any raw generic type automatically!
-            return RAW;
-        } else if (type.getTypeParameters().length == 0) {
-            return NON_GENERIC;
-        } else {
-            return GENERIC;
-        }
+        final Optional<TypeNatures> nature = stream(values()).filter(n -> n.matches(type)).findFirst();
+        assert nature.isPresent() : "A class cannot have a type nature different to non-generic, generic, or raw!";
+        return nature.get();
     }
-
-    private static final List<Class<?>> JAVA8_RAW_GENERIC_TYPES = asList(
-                                                                         /* (1) BeanContext and sub-types */
-                                                                         // extends Collection<NO-TYPE>
-                                                                         java.beans.beancontext.BeanContext.class,
-                                                                         // extends BeanContext
-                                                                         java.beans.beancontext.BeanContextServices.class,
-                                                                         // extends BeanContext
-                                                                         java.beans.beancontext.BeanContextSupport.class,
-                                                                         // extends BeanContextServices,
-                                                                         // BeanContextSupport
-                                                                         java.beans.beancontext.BeanContextServicesSupport.class,
-
-                                                                         /* (2) BasicComboBoxRenderer and sub-types */
-                                                                         // extends
-                                                                         // javax.swing.ListCellRenderer<NO-TYPE>
-                                                                         javax.swing.plaf.basic.BasicComboBoxRenderer.class,
-                                                                         // extends BasicComboBoxRenderer
-                                                                         javax.swing.plaf.basic.BasicComboBoxRenderer.UIResource.class,
-
-                                                                         /* (3) XMLEventReader and sub-types */
-                                                                         // extends Iterator<NO-TYPE>
-                                                                         javax.xml.stream.XMLEventReader.class,
-                                                                         // extends XMLEventReader
-                                                                         javax.xml.stream.util.EventReaderDelegate.class);
 
 }
