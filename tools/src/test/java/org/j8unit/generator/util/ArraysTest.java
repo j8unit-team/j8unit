7ec0140d72428a25b45871e40fc1e3c02ce570fd
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-10 10:48:47 +0100

    Testing the Arrays-Tool

--- tools/src/test/java/org/j8unit/generator/util/ArraysTest.java
+++ tools/src/test/java/org/j8unit/generator/util/ArraysTest.java
@@ -1,5 +1,6 @@
 package org.j8unit.generator.util;
 
+import static java.util.Arrays.asList;
 import static org.j8unit.generator.util.Arrays.arrayAnalysisOf;
 import static org.j8unit.generator.util.Arrays.arrayDimensionOf;
 import static org.j8unit.generator.util.Arrays.baseComponentTypeOf;
@@ -7,68 +8,62 @@ import static org.junit.Assert.assertEquals;
 import java.lang.reflect.Array;
 import java.util.Map;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class ArraysTest {
+
+    @Parameters(name = "{0}")
+    public static Iterable<Object[]> data() {
+        return asList(new Object[][] { { boolean.class, boolean.class, 0 }, //
+                                       { byte.class, byte.class, 0 }, //
+                                       { char.class, char.class, 0 }, //
+                                       { int.class, int.class, 0 }, //
+                                       { long.class, long.class, 0 }, //
+                                       { float.class, float.class, 0 }, //
+                                       { double.class, double.class, 0 }, //
+                                       { void.class, void.class, 0 }, //
+                                       { Object.class, Object.class, 0 }, //
+                                       { Object[].class, Object.class, 1 }, //
+                                       { Object[][][].class, Object.class, 3 }, //
+                                       { Object[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][].class,
+                                         Object.class, 255 }, //
+                                       { Array.newInstance(Object.class, 255).getClass(), Object.class, 255 }, //
+                                       { Map.Entry.class, Map.Entry.class, 0 }, //
+                                       { Map.Entry[].class, Map.Entry.class, 1 }, //
+                                       { Map.Entry[][][].class, Map.Entry.class, 3 }, //
+        });
+    }
+
+    @Parameter(0)
+    public Class<?> clazz;
+
+    @Parameter(1)
+    public Class<?> base;
+
+    @Parameter(2)
+    public int dimension;
 
     @Test
     public void test_arrayAnalysisOf()
     throws Exception {
-        assertEquals(byte.class, arrayAnalysisOf(byte.class).getKey());
-        assertEquals(0, (int) arrayAnalysisOf(byte.class).getValue());
-        assertEquals(byte.class, arrayAnalysisOf(byte[][].class).getKey());
-        assertEquals(2, (int) arrayAnalysisOf(byte[][].class).getValue());
-        assertEquals(Object.class, arrayAnalysisOf(Object.class).getKey());
-        assertEquals(0, (int) arrayAnalysisOf(Object.class).getValue());
-        assertEquals(Object.class, arrayAnalysisOf(Object[].class).getKey());
-        assertEquals(1, (int) arrayAnalysisOf(Object[].class).getValue());
-        assertEquals(Object.class, arrayAnalysisOf(Object[][][].class).getKey());
-        assertEquals(3, (int) arrayAnalysisOf(Object[][][].class).getValue());
-        assertEquals(Map.Entry.class, arrayAnalysisOf(Map.Entry.class).getKey());
-        assertEquals(0, (int) arrayAnalysisOf(Map.Entry.class).getValue());
-        assertEquals(Map.Entry.class, arrayAnalysisOf(Map.Entry[].class).getKey());
-        assertEquals(1, (int) arrayAnalysisOf(Map.Entry[].class).getValue());
-        assertEquals(Map.Entry.class, arrayAnalysisOf(Map.Entry[][][].class).getKey());
-        assertEquals(3, (int) arrayAnalysisOf(Map.Entry[][][].class).getValue());
+        assertEquals(this.base, arrayAnalysisOf(this.clazz).getKey());
+        assertEquals(this.dimension, (int) arrayAnalysisOf(this.clazz).getValue());
     }
 
     @Test
     public void test_baseComponentTypeOf()
     throws Exception {
-        assertEquals(byte.class, baseComponentTypeOf(byte.class));
-        assertEquals(byte.class, baseComponentTypeOf(byte[][].class));
-        assertEquals(Object.class, baseComponentTypeOf(Object.class));
-        assertEquals(Object.class, baseComponentTypeOf(Object[].class));
-        assertEquals(Object.class, baseComponentTypeOf(Object[][][].class));
-        assertEquals(Map.Entry.class, baseComponentTypeOf(Map.Entry.class));
-        assertEquals(Map.Entry.class, baseComponentTypeOf(Map.Entry[].class));
-        assertEquals(Map.Entry.class, baseComponentTypeOf(Map.Entry[][][].class));
+        assertEquals(this.base, baseComponentTypeOf(this.clazz));
     }
 
     @Test
     public void test_arrayDimensionOf()
     throws Exception {
-        assertEquals(0, arrayDimensionOf(byte.class));
-        assertEquals(2, arrayDimensionOf(byte[][].class));
-        assertEquals(0, arrayDimensionOf(Object.class));
-        assertEquals(1, arrayDimensionOf(Object[].class));
-        assertEquals(3, arrayDimensionOf(Object[][][].class));
-        assertEquals(0, arrayDimensionOf(Map.Entry.class));
-        assertEquals(1, arrayDimensionOf(Map.Entry[].class));
-        assertEquals(3, arrayDimensionOf(Map.Entry[][][].class));
-    }
-
-    @Test
-    public void testGetBasecomponentType()
-    throws Exception {
-        assertEquals(Object.class, baseComponentTypeOf(new Object().getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(new Object[] {}.getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(new Object[][] {}.getClass()));
-        assertEquals(Object.class,
-                     baseComponentTypeOf(new Object[][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][][] {}.getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(Array.newInstance(Object.class, 0).getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(Array.newInstance(Object.class, 1).getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(Array.newInstance(Object.class, 2).getClass()));
-        assertEquals(Object.class, baseComponentTypeOf(Array.newInstance(Object.class, 255).getClass()));
+        assertEquals(this.dimension, arrayDimensionOf(this.clazz));
     }
 
 }
