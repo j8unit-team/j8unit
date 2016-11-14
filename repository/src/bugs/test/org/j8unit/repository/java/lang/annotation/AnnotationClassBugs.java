a3a911771f0581aaa65b6fbf6e7760bfd34f5044
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-14 21:11:36 +0100

    Adding some bug explanation (JavaDoc)

--- repository/src/bugs/test/org/j8unit/repository/java/lang/annotation/AnnotationClassBugs.java
+++ repository/src/bugs/test/org/j8unit/repository/java/lang/annotation/AnnotationClassBugs.java
@@ -3,6 +3,15 @@ package org.j8unit.repository.java.lang.annotation;
 import org.junit.Test;
 
 /**
+ * <a href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-9.html#jls-9.6.1">
+ * <q>By virtue of the <em>AnnotationTypeElementDeclaration</em> production, a method declaration in an annotation type
+ * declaration cannot have formal parameters, type parameters, or a {@code throws} clause</q> (JLS,
+ * Sec.&thinsp;9.6.1</a>)!
+ *
+ * Unfortunately, the java compiler translates lambda fields into <em>synthetic</em> methods! In result, the byte code
+ * may contain methods that actually do have formal parameters and/or do have a {@code throws} clause. Because of the
+ * Java's internal assumptions, working with lambda-field-containing annotations causes strange errors.
+ *
  * @since 0.9.4
  */
 public class AnnotationClassBugs {
@@ -13,22 +22,16 @@ public class AnnotationClassBugs {
         awsf.declaredMethodsCannotHaveFormalParameters();
     }
 
-    @Test(expected = AssertionError.class)
-    public void javaCompilerCrashesVirtueOfAnnotationByMethodWithFormalParameters_UnaryOperator() {
-        final AnnotationClassTests<AnnotationWithUnaryOperatorField> awuof = () -> AnnotationWithUnaryOperatorField.class;
-        awuof.declaredMethodsCannotHaveFormalParameters();
-    }
-
-    @Test
-    public void javaCompilerFulfillsVirtueOfAnnotationByMethodWithFormalParameters_Callable() {
-        final AnnotationClassTests<AnnotationWithCallableField> awcf = () -> AnnotationWithCallableField.class;
-        awcf.declaredMethodsCannotHaveFormalParameters();
-    }
-
     @Test
     public void javaCompilerFulfillsVirtueOfAnnotationByMethodWithThrowsClause_Supplier() {
         final AnnotationClassTests<AnnotationWithSupplierField> awsf = () -> AnnotationWithSupplierField.class;
         awsf.declaredMethodsCannotHaveThrowsClause();
+    }
+
+    @Test(expected = AssertionError.class /* because there is a bug! */)
+    public void javaCompilerCrashesVirtueOfAnnotationByMethodWithFormalParameters_UnaryOperator() {
+        final AnnotationClassTests<AnnotationWithUnaryOperatorField> awuof = () -> AnnotationWithUnaryOperatorField.class;
+        awuof.declaredMethodsCannotHaveFormalParameters();
     }
 
     @Test
@@ -37,7 +40,13 @@ public class AnnotationClassBugs {
         awuof.declaredMethodsCannotHaveThrowsClause();
     }
 
-    @Test(expected = AssertionError.class)
+    @Test
+    public void javaCompilerFulfillsVirtueOfAnnotationByMethodWithFormalParameters_Callable() {
+        final AnnotationClassTests<AnnotationWithCallableField> awcf = () -> AnnotationWithCallableField.class;
+        awcf.declaredMethodsCannotHaveFormalParameters();
+    }
+
+    @Test(expected = AssertionError.class /* because there is a bug! */)
     public void javaCompilerCrashesVirtueOfAnnotationByMethodWithThrowsClause_Callable() {
         final AnnotationClassTests<AnnotationWithCallableField> awcf = () -> AnnotationWithCallableField.class;
         awcf.declaredMethodsCannotHaveThrowsClause();
