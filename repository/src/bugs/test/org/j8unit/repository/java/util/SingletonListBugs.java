a96e88e85d8a4246b69150f1c1e8684c2e28f2f2
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-14 17:00:45 +0100

    Put the unmodifiable-bugs in a nutshell

--- repository/src/bugs/test/org/j8unit/repository/java/util/SingletonListBugs.java
+++ repository/src/bugs/test/org/j8unit/repository/java/util/SingletonListBugs.java
@@ -1,35 +1,76 @@
 package org.j8unit.repository.java.util;
 
-import static java.util.Collections.singletonList;
+import static java.lang.String.format;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
 
 /**
- * {@link Collections#singletonList(Object)} is said to be immutable. Thus it should be unmodifiable too. Unfortunately,
+ * {@link Collections#singletonList(Object)} is said to be immutable. Thus, it must be unmodifiable too. Unfortunately,
  * it does not behave accordingly!
+ *
+ * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html">The Java definition
+ *      of <em>unmodifiable</em> and <em>immutable</em> collections</a>
  *
  * @since 0.9.6
  */
 public class SingletonListBugs {
 
     @Test(expected = AssertionError.class)
-    public void singletonListIllegallySupportsRemove() {
-        final UnmodifiableListTests<List<Object>, Object> lt = () -> singletonList("single");
+    public void singletonListIllegallySupportsAddAll() {
+        final List<Object> immutable = Collections.singletonList("single");
         try {
-            lt.removeMustThrowUOE();
-            fail("According to the immutable/unmodifiable definition, a UnsupportedOperationException must have been thrown!");
-        } catch (final UnsupportedOperationException missing) {}
+            immutable.addAll(Collections.emptyList());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonListIllegallySupportsRemove() {
+        final List<Object> immutable = Collections.singletonList("single");
+        try {
+            immutable.remove(new Object());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonListIllegallySupportsRemoveAll() {
+        final List<Object> immutable = Collections.singletonList("single");
+        try {
+            immutable.removeAll(Collections.emptyList());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonListIllegallySupportsRetainAll() {
+        final List<Object> immutable = Collections.singletonList("single");
+        try {
+            immutable.retainAll(Collections.singletonList("single"));
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
     }
 
     @Test(expected = AssertionError.class)
     public void singletonListIllegallySupportsSort() {
-        final UnmodifiableListTests<List<Object>, Object> lt = () -> singletonList("single");
+        final List<Object> immutable = Collections.singletonList("single");
         try {
-            lt.sortMustThrowUOE();
-            fail("According to the immutable/unmodifiable definition, a UnsupportedOperationException must have been thrown!");
-        } catch (final UnsupportedOperationException missing) {}
+            immutable.sort((x, y) -> immutable.indexOf(x) - immutable.indexOf(y));
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
     }
 
 }
--- repository/src/bugs/test/org/j8unit/repository/java/util/SingletonSetBugs.java
+++ repository/src/bugs/test/org/j8unit/repository/java/util/SingletonSetBugs.java
@@ -1,26 +1,65 @@
 package org.j8unit.repository.java.util;
 
-import static java.util.Collections.singleton;
+import static java.lang.String.format;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import java.util.Collections;
 import java.util.Set;
 import org.junit.Test;
 
 /**
- * {@link Collections#singleton(Object)} is said to be immutable. Thus it should be unmodifiable too. Unfortunately, it
+ * {@link Collections#singleton(Object)} is said to be immutable. Thus, it must be unmodifiable too. Unfortunately, it
  * does not behave accordingly!
+ *
+ * @see <a href="https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html">The Java definition
+ *      of <em>unmodifiable</em> and <em>immutable</em> collections</a>
  *
  * @since 0.9.6
  */
 public class SingletonSetBugs {
 
     @Test(expected = AssertionError.class)
-    public void singletonIllegallySupportsRemove() {
-        final UnmodifiableSetTests<Set<Object>, Object> lt = () -> singleton("single");
+    public void singletonSetIllegallySupportsAddAll() {
+        final Set<Object> immutable = Collections.singleton("single");
         try {
-            lt.removeMustThrowUOE();
-            fail("According to the immutable/unmodifiable definition, a UnsupportedOperationException must have been thrown!");
-        } catch (final UnsupportedOperationException missing) {}
+            immutable.addAll(Collections.emptySet());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonSetIllegallySupportsRemove() {
+        final Set<Object> immutable = Collections.singleton("single");
+        try {
+            immutable.remove(new Object());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonSetIllegallySupportsRemoveAll() {
+        final Set<Object> immutable = Collections.singleton("single");
+        try {
+            immutable.removeAll(Collections.emptySet());
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
+    }
+
+    @Test(expected = AssertionError.class)
+    public void singletonSetIllegallySupportsRetainAll() {
+        final Set<Object> immutable = Collections.singleton("single");
+        try {
+            immutable.retainAll(Collections.singleton("single"));
+            fail(format("According to the unmodifiable/immutable definition, an %s must have been thrown!", UnsupportedOperationException.class));
+        } catch (final UnsupportedOperationException unmodifiableBarrier) {
+            assertTrue("A really unmodifiable collection does not support the operation above!", true);
+        }
     }
 
 }
