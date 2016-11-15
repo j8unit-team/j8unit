093d98a6bfd2a228c5d94bc6ff6d520691bd9e3f
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-15 23:16:26 +0100

    static methods are effectively final anyway -- make it explicit

--- core/src/main/java/org/j8unit/Assert.java
+++ core/src/main/java/org/j8unit/Assert.java
@@ -30,31 +30,31 @@ extends org.junit.Assert {
         return () -> Objects.toString(cs, null);
     }
 
-    public static void fail(final CharSequence message) {
+    public static final void fail(final CharSequence message) {
         fail(supply(message));
     }
 
-    public static void assertTrue(final CharSequence message, final boolean condition) {
+    public static final void assertTrue(final CharSequence message, final boolean condition) {
         SupplierBasedAssert.assertTrue(supply(message), condition);
     }
 
-    public static void assertFalse(final CharSequence message, final boolean condition) {
+    public static final void assertFalse(final CharSequence message, final boolean condition) {
         SupplierBasedAssert.assertFalse(supply(message), condition);
     }
 
-    public static void assertNull(final CharSequence message, final Object object) {
+    public static final void assertNull(final CharSequence message, final Object object) {
         SupplierBasedAssert.assertNull(supply(message), object);
     }
 
-    public static void assertNotNull(final CharSequence message, final Object object) {
+    public static final void assertNotNull(final CharSequence message, final Object object) {
         SupplierBasedAssert.assertNotNull(supply(message), object);
     }
 
-    public static void assertSame(final CharSequence message, final Object expected, final Object actual) {
+    public static final void assertSame(final CharSequence message, final Object expected, final Object actual) {
         SupplierBasedAssert.assertSame(supply(message), expected, actual);
     }
 
-    public static void assertNotSame(final CharSequence message, final Object unexpected, final Object actual) {
+    public static final void assertNotSame(final CharSequence message, final Object unexpected, final Object actual) {
         SupplierBasedAssert.assertNotSame(supply(message), unexpected, actual);
     }
 
@@ -74,11 +74,11 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertNotEquals(Supplier, double, double, double)] to compare floating-point numbers!");
     }
 
-    public static void assertEquals(final CharSequence message, final double expected, final double actual, final double delta) {
+    public static final void assertEquals(final CharSequence message, final double expected, final double actual, final double delta) {
         SupplierBasedAssert.assertEquals(supply(message), expected, actual, delta);
     }
 
-    public static void assertNotEquals(final CharSequence message, final double unexpected, final double actual, final double delta) {
+    public static final void assertNotEquals(final CharSequence message, final double unexpected, final double actual, final double delta) {
         SupplierBasedAssert.assertNotEquals(supply(message), unexpected, actual, delta);
     }
 
@@ -98,46 +98,46 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertNotEquals(Supplier, float, float, float)] to compare floating-point numbers!");
     }
 
-    public static void assertEquals(final CharSequence message, final float expected, final float actual, final float delta) {
+    public static final void assertEquals(final CharSequence message, final float expected, final float actual, final float delta) {
         SupplierBasedAssert.assertEquals(supply(message), expected, actual, delta);
     }
 
-    public static void assertNotEquals(final CharSequence message, final float unexpected, final float actual, final float delta) {
+    public static final void assertNotEquals(final CharSequence message, final float unexpected, final float actual, final float delta) {
         SupplierBasedAssert.assertNotEquals(supply(message), unexpected, actual, delta);
     }
 
-    public static void assertEquals(final CharSequence message, final long expected, final long actual) {
+    public static final void assertEquals(final CharSequence message, final long expected, final long actual) {
         SupplierBasedAssert.assertEquals(supply(message), expected, actual);
     }
 
-    public static void assertNotEquals(final CharSequence message, final long unexpected, final long actual) {
+    public static final void assertNotEquals(final CharSequence message, final long unexpected, final long actual) {
         SupplierBasedAssert.assertNotEquals(supply(message), unexpected, actual);
     }
 
-    public static void assertEquals(final CharSequence message, final Object expected, final Object actual) {
+    public static final void assertEquals(final CharSequence message, final Object expected, final Object actual) {
         SupplierBasedAssert.assertEquals(supply(message), expected, actual);
     }
 
-    public static void assertNotEquals(final CharSequence message, final Object unexpected, final Object actual) {
+    public static final void assertNotEquals(final CharSequence message, final Object unexpected, final Object actual) {
         SupplierBasedAssert.assertNotEquals(supply(message), unexpected, actual);
     }
 
     @Deprecated
-    public static void assertEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals) {
+    public static final void assertEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals) {
         SupplierBasedAssert.assertEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final boolean[] expecteds, final boolean[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final boolean[] expecteds, final boolean[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final byte[] expecteds, final byte[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final byte[] expecteds, final byte[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final char[] expecteds, final char[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final char[] expecteds, final char[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
@@ -151,7 +151,7 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertArrayEquals(Supplier, double[], double[], double)] to compare doubleing-point numbers!");
     }
 
-    public static void assertArrayEquals(final CharSequence message, final double[] expecteds, final double[] actuals, final double delta)
+    public static final void assertArrayEquals(final CharSequence message, final double[] expecteds, final double[] actuals, final double delta)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals, delta);
     }
@@ -165,27 +165,27 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertArrayEquals(Supplier, float[], float[], float)] to compare floating-point numbers!");
     }
 
-    public static void assertArrayEquals(final CharSequence message, final float[] expecteds, final float[] actuals, final float delta)
+    public static final void assertArrayEquals(final CharSequence message, final float[] expecteds, final float[] actuals, final float delta)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals, delta);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final int[] expecteds, final int[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final int[] expecteds, final int[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final long[] expecteds, final long[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final long[] expecteds, final long[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final Object[] expecteds, final Object[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final CharSequence message, final short[] expecteds, final short[] actuals)
+    public static final void assertArrayEquals(final CharSequence message, final short[] expecteds, final short[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(supply(message), expecteds, actuals);
     }
@@ -198,31 +198,31 @@ extends org.junit.Assert {
         return (supplier == null) ? null : supply(supplier.get());
     }
 
-    public static void fail(final Supplier<? extends CharSequence> message) {
+    public static final void fail(final Supplier<? extends CharSequence> message) {
         fail(resolve(message));
     }
 
-    public static void assertTrue(final Supplier<? extends CharSequence> message, final boolean condition) {
+    public static final void assertTrue(final Supplier<? extends CharSequence> message, final boolean condition) {
         SupplierBasedAssert.assertTrue(resolve(message), condition);
     }
 
-    public static void assertFalse(final Supplier<? extends CharSequence> message, final boolean condition) {
+    public static final void assertFalse(final Supplier<? extends CharSequence> message, final boolean condition) {
         SupplierBasedAssert.assertFalse(resolve(message), condition);
     }
 
-    public static void assertNull(final Supplier<? extends CharSequence> message, final Object object) {
+    public static final void assertNull(final Supplier<? extends CharSequence> message, final Object object) {
         SupplierBasedAssert.assertNull(resolve(message), object);
     }
 
-    public static void assertNotNull(final Supplier<? extends CharSequence> message, final Object object) {
+    public static final void assertNotNull(final Supplier<? extends CharSequence> message, final Object object) {
         SupplierBasedAssert.assertNotNull(resolve(message), object);
     }
 
-    public static void assertSame(final Supplier<? extends CharSequence> message, final Object expected, final Object actual) {
+    public static final void assertSame(final Supplier<? extends CharSequence> message, final Object expected, final Object actual) {
         SupplierBasedAssert.assertSame(resolve(message), expected, actual);
     }
 
-    public static void assertNotSame(final Supplier<? extends CharSequence> message, final Object unexpected, final Object actual) {
+    public static final void assertNotSame(final Supplier<? extends CharSequence> message, final Object unexpected, final Object actual) {
         SupplierBasedAssert.assertNotSame(resolve(message), unexpected, actual);
     }
 
@@ -242,11 +242,11 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertNotEquals(Supplier, double, double, double)] to compare floating-point numbers!");
     }
 
-    public static void assertEquals(final Supplier<? extends CharSequence> message, final double expected, final double actual, final double delta) {
+    public static final void assertEquals(final Supplier<? extends CharSequence> message, final double expected, final double actual, final double delta) {
         SupplierBasedAssert.assertEquals(resolve(message), expected, actual, delta);
     }
 
-    public static void assertNotEquals(final Supplier<? extends CharSequence> message, final double unexpected, final double actual, final double delta) {
+    public static final void assertNotEquals(final Supplier<? extends CharSequence> message, final double unexpected, final double actual, final double delta) {
         SupplierBasedAssert.assertNotEquals(resolve(message), unexpected, actual, delta);
     }
 
@@ -266,46 +266,46 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertNotEquals(Supplier, float, float, float)] to compare floating-point numbers!");
     }
 
-    public static void assertEquals(final Supplier<? extends CharSequence> message, final float expected, final float actual, final float delta) {
+    public static final void assertEquals(final Supplier<? extends CharSequence> message, final float expected, final float actual, final float delta) {
         SupplierBasedAssert.assertEquals(resolve(message), expected, actual, delta);
     }
 
-    public static void assertNotEquals(final Supplier<? extends CharSequence> message, final float unexpected, final float actual, final float delta) {
+    public static final void assertNotEquals(final Supplier<? extends CharSequence> message, final float unexpected, final float actual, final float delta) {
         SupplierBasedAssert.assertNotEquals(resolve(message), unexpected, actual, delta);
     }
 
-    public static void assertEquals(final Supplier<? extends CharSequence> message, final long expected, final long actual) {
+    public static final void assertEquals(final Supplier<? extends CharSequence> message, final long expected, final long actual) {
         SupplierBasedAssert.assertEquals(resolve(message), expected, actual);
     }
 
-    public static void assertNotEquals(final Supplier<? extends CharSequence> message, final long unexpected, final long actual) {
+    public static final void assertNotEquals(final Supplier<? extends CharSequence> message, final long unexpected, final long actual) {
         SupplierBasedAssert.assertNotEquals(resolve(message), unexpected, actual);
     }
 
-    public static void assertEquals(final Supplier<? extends CharSequence> message, final Object expected, final Object actual) {
+    public static final void assertEquals(final Supplier<? extends CharSequence> message, final Object expected, final Object actual) {
         SupplierBasedAssert.assertEquals(resolve(message), expected, actual);
     }
 
-    public static void assertNotEquals(final Supplier<? extends CharSequence> message, final Object unexpected, final Object actual) {
+    public static final void assertNotEquals(final Supplier<? extends CharSequence> message, final Object unexpected, final Object actual) {
         SupplierBasedAssert.assertNotEquals(resolve(message), unexpected, actual);
     }
 
     @Deprecated
-    public static void assertEquals(final Supplier<? extends CharSequence> message, final Object[] expecteds, final Object[] actuals) {
+    public static final void assertEquals(final Supplier<? extends CharSequence> message, final Object[] expecteds, final Object[] actuals) {
         SupplierBasedAssert.assertEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final boolean[] expecteds, final boolean[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final boolean[] expecteds, final boolean[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final byte[] expecteds, final byte[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final byte[] expecteds, final byte[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final char[] expecteds, final char[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final char[] expecteds, final char[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
@@ -319,7 +319,8 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertArrayEquals(Supplier, double[], double[], double)] to compare doubleing-point numbers!");
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final double[] expecteds, final double[] actuals, final double delta)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final double[] expecteds, final double[] actuals,
+                                               final double delta)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals, delta);
     }
@@ -333,27 +334,28 @@ extends org.junit.Assert {
         Assert.fail("Abandoned method! Instead, use [assertArrayEquals(Supplier, float[], float[], float)] to compare floating-point numbers!");
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final float[] expecteds, final float[] actuals, final float delta)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final float[] expecteds, final float[] actuals,
+                                               final float delta)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals, delta);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final int[] expecteds, final int[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final int[] expecteds, final int[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final long[] expecteds, final long[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final long[] expecteds, final long[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final Object[] expecteds, final Object[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final Object[] expecteds, final Object[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
 
-    public static void assertArrayEquals(final Supplier<? extends CharSequence> message, final short[] expecteds, final short[] actuals)
+    public static final void assertArrayEquals(final Supplier<? extends CharSequence> message, final short[] expecteds, final short[] actuals)
     throws ArrayComparisonFailure {
         SupplierBasedAssert.assertArrayEquals(resolve(message), expecteds, actuals);
     }
