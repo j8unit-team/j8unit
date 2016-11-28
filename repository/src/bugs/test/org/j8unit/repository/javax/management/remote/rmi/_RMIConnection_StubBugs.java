9238bc4de027e4eaae8a5ba235eb6315dd2c8fc6
Stefan Gasterstädt <stefan@informaticum.de>
2016-11-28 19:10:35 +0100

    Focus "RMI" characteristic in favour of CORBA "Stub" (plus try-close)

--- repository/src/bugs/test/org/j8unit/repository/javax/management/remote/rmi/_RMIConnection_StubBugs.java
+++ repository/src/bugs/test/org/j8unit/repository/javax/management/remote/rmi/_RMIConnection_StubBugs.java
@@ -2,24 +2,28 @@ package org.j8unit.repository.javax.management.remote.rmi;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.management.remote.rmi.RMIConnection;
 import javax.management.remote.rmi._RMIConnection_Stub;
-import javax.rmi.CORBA.Stub;
 import org.junit.Test;
 import org.omg.CORBA.BAD_OPERATION;
 
 public class _RMIConnection_StubBugs {
 
     @Test(expected = BAD_OPERATION.class /* because there is a bug! */)
-    public void hashCodeThrowsAnException() {
-        final Stub instance = new _RMIConnection_Stub();
-        instance.hashCode();
+    public void hashCodeThrowsAnException()
+    throws Exception {
+        try (final RMIConnection instance = new _RMIConnection_Stub()) {
+            instance.hashCode();
+        }
     }
 
     @Test(expected = BAD_OPERATION.class /* because there is a bug! */)
-    public void hashSetAddCrashes() {
-        final Set<Stub> set = new HashSet<>();
-        final Stub instance = new _RMIConnection_Stub();
-        set.add(instance);
+    public void hashSetAddCrashes()
+    throws Exception {
+        try (final RMIConnection instance = new _RMIConnection_Stub()) {
+            final Set<RMIConnection> set = new HashSet<>();
+            set.add(instance);
+        }
     }
 
 }
