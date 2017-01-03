4c477f88c747f497d764a444ab3fe051cf5eb6cc
Stefan Gasterstädt <stefan@informaticum.de>
2017-01-03 02:34:58 +0100

    Refactoring the type analysis methods

--- core/src/main/java/org/j8unit/runners/model/J8TestClass.java
+++ core/src/main/java/org/j8unit/runners/model/J8TestClass.java
@@ -1,12 +1,16 @@
 package org.j8unit.runners.model;
 
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.empty;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.Test;
 import org.junit.internal.MethodSorter;
 import org.junit.runners.model.FrameworkField;
@@ -46,21 +50,9 @@ extends TestClass {
     }
 
     /**
-     * For whatever reason, {@link TestClass} does not declare {@link TestClass#getSuperClasses(Class)} in a reusable
-     * way. Thus, we simply specify our own implementation.
-     *
-     * @param c
-     *            the java type to query its class hierarchy
-     * @return the given type and its class hierarchy
-     */
-    public static List<Class<?>> getSuperClasses(Class<?> c) {
-        for (final List<Class<?>> l = new ArrayList<>();; c = c.getSuperclass()) if (c!=null) l.add(c); else return l;
-    }
-
-    /**
      * <p>
      * In addition to the {@link TestClass#scanAnnotatedMembers(Map, Map)}, this method also scans for {@link Test
-     * &#64;Test} annotated {@code default} method of each (either directly or indirectly) implemented
+     * &#64;Test} annotated {@code default} methods of each (either directly or indirectly) implemented
      * {@code interface}. Each of these methods will be added into {@code methodsForAnnotations} if and only if there is
      * no specific {@code class} implementation.
      * </p>
@@ -89,45 +81,224 @@ extends TestClass {
      * @see #addToAnnotationLists(org.junit.runners.model.FrameworkMember, Map)
      */
     protected void scanAnnotatedDefaultMethods(final Map<Class<? extends Annotation>, List<FrameworkMethod>> methodsForAnnotations) {
-        for (final Class<?> eachInterface : getInterfaces(this.getJavaClass())) {
+        for (final Class<?> interfaze : allInterfacesOf(this.getJavaClass())) {
             // inspect each interface's declared methods
-            for (final Method eachMethod : MethodSorter.getDeclaredMethods(eachInterface)) {
-                if (eachMethod.isDefault()) {
-                    addToAnnotationLists(new FrameworkMethod(eachMethod), methodsForAnnotations);
+            for (final Method method : MethodSorter.getDeclaredMethods(interfaze)) {
+                if (method.isDefault()) {
+                    addToAnnotationLists(new FrameworkMethod(method), methodsForAnnotations);
                 }
             }
         }
     }
 
     /**
-     * <p>
-     * In opposite to {@link Class#getInterfaces()} this method returns all (!) {@code interface}s of a given
-     * {@link Class}. The {@link Set} is ordered in the same order the {@code interface}s are implemented.
-     * </p>
+     * Returns a mutable, ordered {@link Set} of all {@code class}es the given Java {@code type} is representing.
      *
-     * @param clazz
-     *            the {@link Class} to return all of its {@code interface}s
-     * @return an ordered {@link Set} of all {@code interface}s implemented by the given {@link Class}
+     * This method is {@code null} safe, meaning it returns an according empty set.
+     *
+     * This method also returns an empty set if the given Java {@code type} is not a true {@code class} but an
+     * {@code interface}.
+     *
+     * @param type
+     *            the Java type to query its {@code class}es
+     * @return all {@code class}es the given Java {@code type} is representing
+     *
+     * @since 4.12.2
      */
-    public static final Set<Class<?>> getInterfaces(final Class<?> clazz) {
-        return getInterfaces(clazz.getInterfaces());
+    public static final LinkedHashSet<Class<?>> allClassesOf(final Class<?> type) {
+        return getClassHierarchy(type);
     }
 
     /**
-     * <p>
-     * Helper method for {@link #getInterfaces(Class)}.
-     * </p>
+     * Returns a mutable, ordered {@link Set} of all {@code interface}s the given Java {@code type} is representing.
+     *
+     * This method is {@code null} safe, meaning it returns an according empty set.
+     *
+     * This method also returns an empty set if neither the given Java {@code type} nor any of its super-classes
+     * implement an {@code interface}.
+     *
+     * @param type
+     *            the Java type to query its {@code interface}s
+     * @return all {@code interface}s the given Java {@code type} is representing
+     *
+     * @since 4.12.2
+     */
+    public static final LinkedHashSet<Class<?>> allInterfacesOf(final Class<?> type) {
+        return getTypeHierarchyAsStream(type).filter(Class::isInterface).collect(toCollection(LinkedHashSet::new));
+    }
+
+    /**
+     * Returns a mutable, ordered {@link Set} of the complete {@code class} hierarchy of the given Java {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according empty set.
+     *
+     * This method also returns an empty set if the given Java {@code type} is not a true {@code class} but an
+     * {@code interface}.
+     *
+     * @apiNote For whatever reason, {@link TestClass} does not declare {@link TestClass#getSuperClasses(Class)} in a
+     *          reusable way. Thus, we simply specify our own hierarchy exploration implementation.
+     *
+     *          In case the given {@code type} represents a true Java {@code class}, this method behaves
+     *          <em>similar</em> to {@link TestClass#getSuperClasses(Class)}. If, otherwise, the given {@code type}
+     *          represents an {@code interface}, this method behaves <em>differently</em>, because the set will be
+     *          empty. In order to explore some interface's hierarchy, use {@link #getInterfaceHierarchy(Class)}
+     *          instead.
+     *
+     * @see #getClassHierarchyAsStream(Class)
+     * @see #getInterfaceHierarchy(Class)
+     * @see #getTypeHierarchy(Class)
+     *
+     * @param type
+     *            the Java {@code class} to query its class hierarchy
+     * @return the class' hierarchy
+     *
+     * @since 4.12.2
+     */
+    public static final LinkedHashSet<Class<?>> getClassHierarchy(final Class<?> type) {
+        return getClassHierarchyAsStream(type).collect(toCollection(LinkedHashSet::new));
+    }
+
+    /**
+     * Returns a {@link Stream} of the complete {@code class} hierarchy of the given Java {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according {@linkplain Stream#empty() empty} stream.
+     *
+     * This method also returns an empty stream if the given Java {@code type} is not a true {@code class} but an
+     * {@code interface}.
      *
      * @implSpec Java does not allow cyclic type hierarchy. Thus, this recursive implementation will definitely come to
      *           an end.
+     *
+     * @see #getClassHierarchy(Class)
+     * @see #getInterfaceHierarchyAsStream(Class)
+     * @see #getTypeHierarchyAsStream(Class)
+     *
+     * @param type
+     *            the Java {@code class} to query its class hierarchy
+     * @return the class' hierarchy
+     *
+     * @since 4.12.2
      */
-    private static final Set<Class<?>> getInterfaces(final Class<?>[] allInterfaces) {
-        final Set<Class<?>> results = new LinkedHashSet<>();
-        for (final Class<?> eachInterface : allInterfaces) {
-            results.add(eachInterface);
-            results.addAll(getInterfaces(eachInterface.getInterfaces()));
+    public static final Stream<Class<?>> getClassHierarchyAsStream(final Class<?> type) {
+        if (type == null) {
+            return empty();
+        } else if (type.isInterface()) {
+            return empty();
+        } else {
+            return concat(Stream.of(type), getClassHierarchyAsStream(type.getSuperclass()));
         }
-        return results;
+    }
+
+    /**
+     * Returns a mutable, ordered {@link Set} of the complete {@code interface} hierarchy of the given Java
+     * {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according empty set.
+     *
+     * This method also returns an empty set if the given Java {@code type} is not a true {@code interface} but a
+     * {@code class}.
+     *
+     * @see #getInterfaceHierarchyAsStream(Class)
+     * @see #getClassHierarchy(Class)
+     * @see #getTypeHierarchy(Class)
+     *
+     * @param type
+     *            the Java {@code interface} to query its interface hierarchy
+     * @return the interface's hierarchy
+     *
+     * @since 4.12.2
+     */
+    public static final LinkedHashSet<Class<?>> getInterfaceHierarchy(final Class<?> type) {
+        return getInterfaceHierarchyAsStream(type).collect(toCollection(LinkedHashSet::new));
+    }
+
+    /**
+     * Returns a {@link Stream} of the complete {@code interface} hierarchy of the given Java {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according {@linkplain Stream#empty() empty} stream.
+     *
+     * This method also returns an empty stream if the given Java {@code type} is not a true {@code interface} but a
+     * {@code class}.
+     *
+     * @implSpec Java does not allow cyclic type hierarchy. Thus, this recursive implementation will definitely come to
+     *           an end.
+     *
+     * @see #getInterfaceHierarchy(Class)
+     * @see #getClassHierarchyAsStream(Class)
+     * @see #getTypeHierarchyAsStream(Class)
+     *
+     * @param type
+     *            the Java {@code interface} to query its interface hierarchy
+     * @return the interface's hierarchy
+     *
+     * @since 4.12.2
+     */
+    public static final Stream<Class<?>> getInterfaceHierarchyAsStream(final Class<?> i) {
+        if (i == null) {
+            return empty();
+        } else if (!i.isInterface()) {
+            return empty();
+        } else {
+            return concat(Stream.of(i), stream(i.getInterfaces()).flatMap(x -> getInterfaceHierarchyAsStream(x)));
+        }
+    }
+
+    /**
+     * Returns a mutable, ordered {@link Set} of the complete type hierarchy of the given Java {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according empty set.
+     *
+     * If the given {@code type} represents a true {@code interface}, this method returns the according
+     * {@linkplain #getInterfaceHierarchy(Class) interface hierarchy}.
+     *
+     * If the given {@code type} represents a true {@code code}, this method returns the complete
+     * {@linkplain #getClassHierarchy(Class) class hierarchy} whereas each class is immediately followed by
+     * {@linkplain #getInterfaceHierarchy(Class) all interfaces} it represents.
+     *
+     * @see #getTypeHierarchyAsStream(Class)
+     * @see #getClassHierarchy(Class)
+     * @see #getInterfaceHierarchy(Class)
+     *
+     * @param type
+     *            the Java type to query its type hierarchy
+     * @return the type's hierarchy
+     *
+     * @since 4.12.2
+     */
+    public static final LinkedHashSet<Class<?>> getTypeHierarchy(final Class<?> type) {
+        return getTypeHierarchyAsStream(type).collect(toCollection(LinkedHashSet::new));
+    }
+
+    /**
+     * Returns a {@link Stream} of the complete type hierarchy of the given Java {@code type}.
+     *
+     * This method is {@code null} safe, meaning it returns an according {@linkplain Stream#empty() empty} stream.
+     *
+     * If the given {@code type} represents a true {@code interface}, this method returns the according
+     * {@linkplain #getInterfaceHierarchyAsStream(Class) interface hierarchy}.
+     *
+     * If the given {@code type} represents a true {@code code}, this method returns the complete
+     * {@linkplain #getClassHierarchyAsStream(Class) class hierarchy} whereas each class is immediately followed by
+     * {@linkplain #getInterfaceHierarchyAsStream(Class) all interfaces} it represents.
+     *
+     * @see #getTypeHierarchy(Class)
+     * @see #getClassHierarchyAsStream(Class)
+     * @see #getInterfaceHierarchyAsStream(Class)
+     *
+     * @param type
+     *            the Java type to query its class hierarchy
+     * @return the type's hierarchy
+     *
+     * @since 4.12.2
+     */
+    public static final Stream<Class<?>> getTypeHierarchyAsStream(final Class<?> type) {
+        if (type == null) {
+            return empty();
+        } else if (type.isInterface()) {
+            return getInterfaceHierarchyAsStream(type);
+        } else {
+            return getClassHierarchyAsStream(type).flatMap(t -> concat(Stream.of(t), stream(t.getInterfaces()).flatMap(x -> getInterfaceHierarchyAsStream(x))));
+        }
     }
 
 }
