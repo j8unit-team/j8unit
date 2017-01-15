1706e69c850519ab5dd7b5594c5b68ff6e6b1139
Stefan Gasterstädt <stefan@informaticum.de>
2017-01-15 16:39:20 +0100

    Run more tests

--- core/src/test/java/org/j8unit/AssertBySupplierTest.java
+++ core/src/test/java/org/j8unit/AssertBySupplierTest.java
@@ -1,11 +1,34 @@
 package org.j8unit;
 
+import static java.util.Arrays.asList;
+import java.nio.CharBuffer;
+import java.util.function.Supplier;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 @SuppressWarnings("deprecation")
+@RunWith(Parameterized.class)
 public class AssertBySupplierTest {
 
-    private String someExpensiveMessageCreation() {
+    @Parameters(name = "{index}: {0}")
+    public static Iterable<Object[]> data() {
+        return asList(new Object[][] { //
+                                       { null }, //
+                                       { (Supplier<String>) "FAILED"::toString }, //
+                                       { (Supplier<StringBuilder>) () -> new StringBuilder("FAILED") }, //
+                                       { (Supplier<StringBuffer>) () -> new StringBuffer("FAILED") }, //
+                                       { (Supplier<CharBuffer>) () -> CharBuffer.wrap("FAILED") }, //
+                                       { (Supplier<String>) () -> someExpensiveMessageCreation() } //
+        });
+    }
+
+    @Parameter(0)
+    public Supplier<? extends CharSequence> SUP;
+
+    private static String someExpensiveMessageCreation() {
         // do some very expensive message creation ...
         final String msg = "Damn, test failed!";
         // ... and return the result
@@ -16,332 +39,332 @@ public class AssertBySupplierTest {
     public void test_assertArrayEquals_boolean_success()
     throws Exception {
         final boolean[] array = new boolean[] { true, false };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_boolean_failure()
     throws Exception {
         final boolean[] array = new boolean[] { true, false };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new boolean[] {});
+        Assert.assertArrayEquals(this.SUP, array, new boolean[] {});
     }
 
     @Test
     public void test_assertArrayEquals_byte_success()
     throws Exception {
         final byte[] array = new byte[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_byte_failure()
     throws Exception {
         final byte[] array = new byte[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new byte[] {});
+        Assert.assertArrayEquals(this.SUP, array, new byte[] {});
     }
 
     @Test
     public void test_assertArrayEquals_char_success()
     throws Exception {
         final char[] array = new char[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_char_failure()
     throws Exception {
         final char[] array = new char[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new char[] {});
+        Assert.assertArrayEquals(this.SUP, array, new char[] {});
     }
 
     @Test
     public void test_assertArrayEquals_double_success()
     throws Exception {
         final double[] array = new double[] { 0.0d, 1.0d };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array, 0.0d);
+        Assert.assertArrayEquals(this.SUP, array, array, 0.0d);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_double_failure()
     throws Exception {
         final double[] array = new double[] { 0.0d, 1.0d };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new double[] {}, 0.0d);
+        Assert.assertArrayEquals(this.SUP, array, new double[] {}, 0.0d);
     }
 
     @Test
     public void test_assertArrayEquals_float_success()
     throws Exception {
         final float[] array = new float[] { 0.0f, 1.0f };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array, 0.0f);
+        Assert.assertArrayEquals(this.SUP, array, array, 0.0f);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_float_failure()
     throws Exception {
         final float[] array = new float[] { 0.0f, 1.0f };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new float[] {}, 0.0f);
+        Assert.assertArrayEquals(this.SUP, array, new float[] {}, 0.0f);
     }
 
     @Test
     public void test_assertArrayEquals_int_success()
     throws Exception {
         final int[] array = new int[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_int_failure()
     throws Exception {
         final int[] array = new int[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new int[] {});
+        Assert.assertArrayEquals(this.SUP, array, new int[] {});
     }
 
     @Test
     public void test_assertArrayEquals_long_success()
     throws Exception {
         final long[] array = new long[] { 0l, 1l };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_long_failure()
     throws Exception {
         final long[] array = new long[] { 0l, 1l };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new long[] {});
+        Assert.assertArrayEquals(this.SUP, array, new long[] {});
     }
 
     @Test
     public void test_assertArrayEquals_Object_success()
     throws Exception {
         final Object[] array = new Object[] { "hello", "world" };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_Object_failure()
     throws Exception {
         final Object[] array = new Object[] { "hello", "world" };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new Object[] {});
+        Assert.assertArrayEquals(this.SUP, array, new Object[] {});
     }
 
     @Test
     public void test_assertArrayEquals_short_success()
     throws Exception {
         final short[] array = new short[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertArrayEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertArrayEquals_short_failure()
     throws Exception {
         final short[] array = new short[] { 0, 1 };
-        Assert.assertArrayEquals(() -> someExpensiveMessageCreation(), array, new short[] {});
+        Assert.assertArrayEquals(this.SUP, array, new short[] {});
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_double()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0d, 0.0d);
+        Assert.assertEquals(this.SUP, 0.0d, 0.0d);
     }
 
     @Test
     public void test_assertEquals_double_success()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0d, 0.0d, 0.0d);
+        Assert.assertEquals(this.SUP, 0.0d, 0.0d, 0.0d);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_double_failure()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0d, 1.0d, 0.0d);
+        Assert.assertEquals(this.SUP, 0.0d, 1.0d, 0.0d);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_float()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0f, 0.0f);
+        Assert.assertEquals(this.SUP, 0.0f, 0.0f);
     }
 
     @Test
     public void test_assertEquals_float_success()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0f, 0.0f, 0.0f);
+        Assert.assertEquals(this.SUP, 0.0f, 0.0f, 0.0f);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_float_failure()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0.0f, 1.0f, 0.0f);
+        Assert.assertEquals(this.SUP, 0.0f, 1.0f, 0.0f);
     }
 
     @Test
     public void test_assertEquals_long_success()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0l, 0l);
+        Assert.assertEquals(this.SUP, 0l, 0l);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_long_failure()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), 0l, 1l);
+        Assert.assertEquals(this.SUP, 0l, 1l);
     }
 
     @Test
     public void test_assertEquals_Object_success()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), "hello", "hello");
+        Assert.assertEquals(this.SUP, "hello", "hello");
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_Object_failure()
     throws Exception {
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), "hello", "world");
+        Assert.assertEquals(this.SUP, "hello", "world");
     }
 
     @Test
     public void test_assertEquals_ObjectArray_success()
     throws Exception {
         final Object[] array = new Object[] { "hello", "world" };
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), array, array);
+        Assert.assertEquals(this.SUP, array, array);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertEquals_ObjectArray_failure()
     throws Exception {
         final Object[] array = new Object[] { "hello", "world" };
-        Assert.assertEquals(() -> someExpensiveMessageCreation(), array, new Object[] {});
+        Assert.assertEquals(this.SUP, array, new Object[] {});
     }
 
     @Test
     public void test_assertFalse_success()
     throws Exception {
-        Assert.assertFalse(() -> someExpensiveMessageCreation(), false);
+        Assert.assertFalse(this.SUP, false);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertFalse_failure()
     throws Exception {
-        Assert.assertFalse(() -> someExpensiveMessageCreation(), true);
+        Assert.assertFalse(this.SUP, true);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_double()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0d, 0.0d);
+        Assert.assertNotEquals(this.SUP, 0.0d, 0.0d);
     }
 
     @Test
     public void test_assertNotEquals_double_success()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0d, 1.0d, 0.0d);
+        Assert.assertNotEquals(this.SUP, 0.0d, 1.0d, 0.0d);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_double_failure()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0d, 0.0d, 0.0d);
+        Assert.assertNotEquals(this.SUP, 0.0d, 0.0d, 0.0d);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_float()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0f, 0.0f);
+        Assert.assertNotEquals(this.SUP, 0.0f, 0.0f);
     }
 
     @Test
     public void test_assertNotEquals_float_success()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0f, 1.0f, 0.0f);
+        Assert.assertNotEquals(this.SUP, 0.0f, 1.0f, 0.0f);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_float_failure()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0.0f, 0.0f, 0.0f);
+        Assert.assertNotEquals(this.SUP, 0.0f, 0.0f, 0.0f);
     }
 
     @Test
     public void test_assertNotEquals_long_success()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0l, 1l);
+        Assert.assertNotEquals(this.SUP, 0l, 1l);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_long_failure()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), 0l, 0l);
+        Assert.assertNotEquals(this.SUP, 0l, 0l);
     }
 
     @Test
     public void test_assertNotEquals_Object_success()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), "hello", "world");
+        Assert.assertNotEquals(this.SUP, "hello", "world");
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotEquals_Object_failure()
     throws Exception {
-        Assert.assertNotEquals(() -> someExpensiveMessageCreation(), "hello", "hello");
+        Assert.assertNotEquals(this.SUP, "hello", "hello");
     }
 
     @Test
     public void test_assertNotNull_success()
     throws Exception {
-        Assert.assertNotNull(() -> someExpensiveMessageCreation(), "hello");
+        Assert.assertNotNull(this.SUP, "hello");
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotNull_failure()
     throws Exception {
-        Assert.assertNotNull(() -> someExpensiveMessageCreation(), null);
+        Assert.assertNotNull(this.SUP, null);
     }
 
     @Test
     public void test_assertNotSame_success()
     throws Exception {
-        Assert.assertNotSame(() -> someExpensiveMessageCreation(), "hello", "world");
+        Assert.assertNotSame(this.SUP, "hello", "world");
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNotSame_failure()
     throws Exception {
-        Assert.assertNotSame(() -> someExpensiveMessageCreation(), "hello", "hello");
+        Assert.assertNotSame(this.SUP, "hello", "hello");
     }
 
     @Test
     public void test_assertNull_success()
     throws Exception {
-        Assert.assertNull(() -> someExpensiveMessageCreation(), null);
+        Assert.assertNull(this.SUP, null);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertNull_failure()
     throws Exception {
-        Assert.assertNull(() -> someExpensiveMessageCreation(), "hello");
+        Assert.assertNull(this.SUP, "hello");
     }
 
     @Test
     public void test_assertSame_success()
     throws Exception {
-        Assert.assertSame(() -> someExpensiveMessageCreation(), "hello", "hello");
+        Assert.assertSame(this.SUP, "hello", "hello");
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertSame_failure()
     throws Exception {
-        Assert.assertSame(() -> someExpensiveMessageCreation(), "hello", "world");
+        Assert.assertSame(this.SUP, "hello", "world");
     }
 
     @Test
     public void test_assertTrue_success()
     throws Exception {
-        Assert.assertTrue(() -> someExpensiveMessageCreation(), true);
+        Assert.assertTrue(this.SUP, true);
     }
 
     @Test(expected = AssertionError.class)
     public void test_assertTrue_failure()
     throws Exception {
-        Assert.assertTrue(() -> someExpensiveMessageCreation(), false);
+        Assert.assertTrue(this.SUP, false);
     }
 
 }
