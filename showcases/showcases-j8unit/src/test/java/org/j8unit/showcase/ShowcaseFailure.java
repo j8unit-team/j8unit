4d4cb1765b6489a3d2d2083bf6e99b58eb750540
Stefan Gasterstädt <stefan@informaticum.de>
2017-08-01 00:25:06 +0200

    Move showcases into separate repository
    see: https://github.com/j8unit-team/showcases

--- showcases/showcases-j8unit/src/test/java/org/j8unit/showcase/ShowcaseFailure.java
+++
@@ -1,8 +0,0 @@
-package org.j8unit.showcase;
-
-/**
- * Marker interface to tag intentional showcase failures (enabling JUnit exclusion). If, otherwise, not excluded the
- * maven build will abort by failing tests.
- */
-public abstract interface ShowcaseFailure {
-}
--- showcases/showcases-jm/src/main/java/org/j8unit/showcase/jm/Foobar.java
+++
@@ -1,7 +0,0 @@
-package org.j8unit.showcase.jm;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Foobar {
-}
