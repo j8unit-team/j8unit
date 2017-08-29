64ecd7b80f59c60a96d611726fb021935b95631e
Stefan Gasterstädt <stefan@informaticum.de>
2017-08-29 23:10:14 +0200

    Whoops. Added missing "final" keyword

--- core/src/main/java/org/junit/SupplierBasedAssert.java
+++ core/src/main/java/org/junit/SupplierBasedAssert.java
@@ -764,7 +764,7 @@ extends Assert {
      * @throws AssertionError
      *             iff the assertion fails
      */
-    public static <T> void assertThat(final Supplier<? extends CharSequence> reason, final T actual, final Matcher<? super T> matcher)
+    public static final <T> void assertThat(final Supplier<? extends CharSequence> reason, final T actual, final Matcher<? super T> matcher)
     throws AssertionError {
         // TODO (Issue #4): Provide implementation which requests the supplied fail message only if the assertion fails
         assertThat(resolve(reason), actual, matcher);
