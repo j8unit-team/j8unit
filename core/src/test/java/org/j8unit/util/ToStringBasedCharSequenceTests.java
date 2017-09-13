a8b232c169ef7f62b65f0f2d1c9125b3c17556b1
Stefan Gasterstädt <stefan@informaticum.de>
2017-09-13 09:31:33 +0200

    Clearifying ToStringBasedCharSequenceTests

--- core/src/test/java/org/j8unit/util/ToStringBasedCharSequenceTests.java
+++ core/src/test/java/org/j8unit/util/ToStringBasedCharSequenceTests.java
@@ -6,37 +6,38 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.j8unit.util.helper.FailMessages;
 
 @RunWith(Parameterized.class)
 public class ToStringBasedCharSequenceTests {
 
     @Parameters(name = "{index}: {0}")
-    public static Object[] data() {
+    public static ToStringBasedCharSequence[] testParameters() {
         return FailMessages.values();
     }
 
     @Parameter(0)
-    public FailMessages fm;
+    public ToStringBasedCharSequence seq;
 
     @Test
     public void testSubSequence()
     throws Exception {
-        final String string = this.fm.toString();
-        assertEquals(string, this.fm.subSequence(0, string.length()));
+        final String string = this.seq.toString();
+        assertEquals(string, this.seq.subSequence(0, string.length()));
     }
 
     @Test
     public void testLength()
     throws Exception {
-        final String string = this.fm.toString();
-        assertEquals(string.length(), this.fm.length());
+        final String string = this.seq.toString();
+        assertEquals(string.length(), this.seq.length());
     }
 
     @Test
     public void testCharAt()
     throws Exception {
-        final String string = this.fm.toString();
-        assertEquals(string.charAt(0), this.fm.charAt(0));
+        final String string = this.seq.toString();
+        assertEquals(string.charAt(0), this.seq.charAt(0));
     }
 
 }
