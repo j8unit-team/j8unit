862a3499f3d7a1bafd10eb3fb33be97e42412a96
Stefan Gasterstädt <stefan@informaticum.de>
2017-10-05 11:16:32 +0200

    Refactoring method-overrides-comparison

--- core/src/test/java/org/j8unit/util/ReflectionTests.java
+++ core/src/test/java/org/j8unit/util/ReflectionTests.java
@@ -12,7 +12,7 @@ import static org.j8unit.util.Reflection.allTypesOf;
 import static org.j8unit.util.Reflection.getClassHierarchy;
 import static org.j8unit.util.Reflection.getInterfaceHierarchy;
 import static org.j8unit.util.Reflection.getTypeHierarchy;
-import static org.j8unit.util.Reflection.isAssignableFrom;
+import static org.j8unit.util.Reflection.isOverriddenBy;
 import static org.j8unit.util.Reflection.redundantTypes;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -26,6 +26,7 @@ import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.Inet4Address;
 import java.security.PrivilegedAction;
 import java.text.StringCharacterIterator;
 import java.util.AbstractCollection;
@@ -360,29 +361,79 @@ extends EmptyClass {
     }
 
     /*
-     * Method equality:
+     * Method assignability:
      */
 
     @Test
-    public void testMethodAssignabilityForAbstractMethodWithMoreSpecificReturnType()
+    public void testMethodAssignabilityForSpecificOverriddenAbstractMethod()
     throws Exception {
-        final Method base = Foo.class.getMethod("foo", new Class<?>[] { Number.class });
-        final Method sub = Bar.class.getMethod("foo", new Class<?>[] { Number.class });
-        assertTrue(isAssignableFrom(base, base));
-        assertTrue(isAssignableFrom(base, sub));
-        assertTrue(isAssignableFrom(sub, sub));
-        assertFalse(isAssignableFrom(sub, base));
+        assertOverriddenMethod("someAbstractMethodThatShallBeOverridden");
     }
 
     @Test
     public void testMethodAssignabilityForSpecificOverriddenMethod()
     throws Exception {
-        final Method base = Foo.class.getDeclaredMethod("sayYourName");
-        final Method sub = Bar.class.getMethod("sayYourName");
-        assertTrue(isAssignableFrom(base, base));
-        assertTrue(isAssignableFrom(base, sub));
-        assertTrue(isAssignableFrom(sub, sub));
-        assertFalse(isAssignableFrom(sub, base));
+        assertOverriddenMethod("someProtectedMethodThatShallBeOverridden");
+    }
+
+    @Test
+    public void testMethodAssignabilityForSpecificHiddenMethod()
+    throws Exception {
+        assertNotOverriddenMethod("someStaticMethodThatShallBeHidden");
+    }
+
+    @Test
+    public void testMethod_XXX()
+    throws Exception {
+        assertOverriddenMethod("someMethodThatShallBeRedefinedWithCovariantReturnType");
+    }
+
+    @Test
+    public void testMethod_YYY()
+    throws Exception {
+        assertNotOverriddenMethod("someMethodThatShallBeRedefinedWithContravariantParameterType", Integer.class, Number.class);
+    }
+
+    @Test
+    public void testMethod_ZZZ()
+    throws Exception {
+        assertNotOverriddenMethod("someMethodThatShallBeRedefinedWithCovariantReturnTypeAndContravariantParameterType", Integer.class, Number.class);
+    }
+
+    @Test
+    public void testMethod_AAA()
+    throws Exception {
+        assertNotOverriddenMethod("someMethodThatRedefinedWithDifferentParameterType", String.class, Inet4Address.class);
+    }
+
+    private static void assertOverriddenMethod(final String name)
+    throws Exception {
+        final Method base = Foo.class.getDeclaredMethod(name);
+        final Method sub = Bar.class.getDeclaredMethod(name);
+        assertTrue(isOverriddenBy(base, base));
+        assertTrue(isOverriddenBy(base, sub));
+        assertTrue(isOverriddenBy(sub, sub));
+        assertFalse(isOverriddenBy(sub, base));
+    }
+
+    private static void assertNotOverriddenMethod(final String name)
+    throws Exception {
+        final Method base = Foo.class.getDeclaredMethod(name);
+        final Method sub = Bar.class.getDeclaredMethod(name);
+        assertTrue(isOverriddenBy(base, base));
+        assertFalse(isOverriddenBy(base, sub));
+        assertTrue(isOverriddenBy(sub, sub));
+        assertFalse(isOverriddenBy(sub, base));
+    }
+
+    private static void assertNotOverriddenMethod(final String name, final Class<?> basePT, final Class<?> oPT)
+    throws Exception {
+        final Method base = Foo.class.getDeclaredMethod(name, basePT);
+        final Method sub = Bar.class.getDeclaredMethod(name, oPT);
+        assertTrue(isOverriddenBy(base, base));
+        assertFalse(isOverriddenBy(base, sub));
+        assertTrue(isOverriddenBy(sub, sub));
+        assertFalse(isOverriddenBy(sub, base));
     }
 
     /*
